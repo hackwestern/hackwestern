740f10d8658ba14aabf030629ad97eb41fe10a4b
LordExodius <oscar45697@gmail.com>
2024-05-17 22:20:45 -0400

    add password reset token logic

--- src/server/api/routers/login.ts
+++ src/server/api/routers/login.ts
@@ -1,9 +1,57 @@
 import { z } from "zod";
-
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
-export const loginRouter = createTRPCRouter({
-  reset: protectedProcedure.mutation(({ ctx }) => {
-    // TODO: handler body
+// Imports for reset
+import { randomBytes } from "crypto";
+import { db } from "~/server/db";
+import { resetPasswordTokens, users } from "~/server/db/schema";
+import { eq } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
+
+const TOKEN_EXPIRY = 1000 * 60 * 10; // 10 minutes
+
+export const login = createTRPCRouter({
+  reset: protectedProcedure
+    .input(z.object({ email: z.string() }))
+    .mutation(async ({ input }) => {
+
+      // Fetch user by email
+      const user = await db.query.users.findFirst({
+        columns: { id: true },
+        where: eq(users.email, input.email)
+        });
+
+      if (!user) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "User not found",
+        })
+      }
+
+      const uid = user.id;
+
+      // Generate reset token
+      const resetToken = randomBytes(20).toString("hex"); // 20 byte hex string
+      
+      // Store reset token and expiry; replace existing if exists
+      await db.insert(resetPasswordTokens)
+        .values({
+          userId: uid, 
+          token: resetToken, 
+          expires: new Date(Date.now() + TOKEN_EXPIRY)})
+        .onConflictDoUpdate({ 
+          target: resetPasswordTokens.userId, 
+          set: { 
+            token: resetToken, 
+            expires: new Date(Date.now() + TOKEN_EXPIRY)}
+      });
+
+      const resetLink = `https://hackwestern.com/login/set-password?token=${resetToken}`;
+
+      // TODO: send email with reset link
+
+      return {
+        success: true,
+      };
   }),
 });
