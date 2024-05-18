b51437c6155f9afe148e790b32e305800391ec8a
Hunter Chen <kchen762@uwo.ca>
2024-05-18 14:20:23 -0400

    add get application query

--- src/server/api/routers/application.ts
+++ src/server/api/routers/application.ts
@@ -1,10 +1,27 @@
-import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import { eq } from "drizzle-orm";
 
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { applications } from "~/server/db/schema";
 
 export const applicationRouter = createTRPCRouter({
   get: protectedProcedure.query(({ ctx }) => {
-    // TODO: finish body
+    const userId = ctx.session.user.id;
+    ctx.db.select().from(applications).where(eq(applications.userId, userId)).limit(1)
+      .then((application) => {
+        if (!application.length) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Application not found",
+          });
+        }
+        return application[0];
+      }).catch((error) => {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to fetch application: " + JSON.stringify(error),
+        });
+      });
   }),
 
   save: protectedProcedure.mutation(({ ctx }) => {
