ced1824e17661f75f9e9a73c5c062941be75c94e
Hunter <hello@hunterchen.ca>
2024-05-21 00:33:12 -0400

    add get application query (#13)
    * add get application query
    
    * handle NOT_FOUND error
    
    * add back applicationSaveSchema

--- src/server/api/routers/application.ts
+++ src/server/api/routers/application.ts
@@ -1,17 +1,38 @@
+import { TRPCError } from "@trpc/server";
+import { eq } from "drizzle-orm";
+
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { applications } from "~/server/db/schema";
 import { createInsertSchema } from "drizzle-zod";
 
 const applicationSaveSchema = createInsertSchema(applications)
-  .omit({
-    createdAt: true,
-    updatedAt: true,
-  })
-  .required({ id: true, userId: true });
+.omit({
+  createdAt: true,
+  updatedAt: true,
+})
+.required({ id: true, userId: true });
 
 export const applicationRouter = createTRPCRouter({
   get: protectedProcedure.query(({ ctx }) => {
-    // TODO: finish body
+    const userId = ctx.session.user.id;
+    ctx.db.select().from(applications).where(eq(applications.userId, userId)).limit(1)
+      .then((application) => {
+        if (!application.length) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Application not found",
+          });
+        }
+        return application[0];
+      }).catch((error) => {
+        if (error instanceof TRPCError && error.code === "NOT_FOUND") {
+          throw error;
+        }
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to fetch application: " + JSON.stringify(error),
+        });
+      });
   }),
 
   save: protectedProcedure
