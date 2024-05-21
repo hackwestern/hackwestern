b4def849bbc894387d12703a5269f9bb74fc8bac
basokant <basokanthan@gmail.com>
2024-05-20 22:57:24 -0400

    add drizzle-zod, and create application save input schema using it

--- src/server/api/routers/application.ts
+++ src/server/api/routers/application.ts
@@ -1,13 +1,22 @@
-import { z } from "zod";
-
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { applications } from "~/server/db/schema";
+import { createInsertSchema } from "drizzle-zod";
+
+const applicationSaveSchema = createInsertSchema(applications)
+  .omit({
+    createdAt: true,
+    updatedAt: true,
+  })
+  .required({ id: true, userId: true });
 
 export const applicationRouter = createTRPCRouter({
   get: protectedProcedure.query(({ ctx }) => {
     // TODO: finish body
   }),
 
-  save: protectedProcedure.mutation(({ ctx }) => {
-    // TODO: handler body
-  }),
+  save: protectedProcedure
+    .input(applicationSaveSchema)
+    .mutation(({ input, ctx }) => {
+      // TODO: handler body
+    }),
 });
