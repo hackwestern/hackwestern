0609d50cf7add4f6da2e48effa7a597a304d9d4b
Ben Asokanthan <70033855+basokant@users.noreply.github.com>
2024-05-25 01:10:57 -0400

    Update to drizzle-kit v0.21 (#22)
    * update to drizzle-kit 0.21
    
    * change a to Link
    
    * Update test-and-build.yml
    
    ---------
    
    Co-authored-by: Hunter <hello@hunterchen.ca>

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { type Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 import { env } from "~/env";
 
-export default {
+export default defineConfig({
   schema: "./src/server/db/schema.ts",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    url: env.DATABASE_URL,
   },
   tablesFilter: ["hackwestern2024_*"],
-} satisfies Config;
+});
--- src/pages/api/preregistration/all.ts
+++ src/pages/api/preregistration/all.ts
@@ -1,5 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
+import type { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 import { getServerAuthSession } from "~/server/auth";
 import { db } from "~/server/db";
