c0762ab972ecffb0ef951311ef3f896d22bc7f17
basokant <basokanthan@gmail.com>
2024-05-26 22:02:12 -0400

    add vitest unit tests
    more
    
    add vitest dependencies
    
    add dotenv dependency

---
+++ src/server/api/routers/application.test.ts
@@ -0,0 +1,32 @@
+import { Session } from "next-auth";
+import { describe, expect, it } from "vitest";
+import { createCaller } from "~/server/api/root";
+import { createInnerTRPCContext } from "~/server/api/trpc";
+import { db, type Database } from "~/server/db";
+import { users } from "~/server/db/schema";
+import { UserSeeder } from "~/server/db/seed/userSeeder";
+
+async function mockSession(db: Database): Promise<Session> {
+  const user = new UserSeeder().createRandom();
+  await db.insert(users).values(user).returning();
+
+  return {
+    user,
+    expires: new Date(Date.now() + 10).toISOString(),
+  };
+}
+
+describe("application.get", async () => {
+  const session = await mockSession(db);
+
+  const ctx = createInnerTRPCContext({ session, db });
+  const caller = createCaller(ctx);
+
+  it("should throw an error if no session exists", async () => {
+    expect(() => caller.application.get()).toThrowError();
+  });
+
+  it("should throw an error if no application exists", async () => {
+    expect(() => caller.application.get()).toThrowError();
+  });
+});
