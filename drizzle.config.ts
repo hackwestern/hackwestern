d7a0b575b053cccbee76603dfbfef2d834eddc04
Hunter <hello@hunterchen.ca>
2024-05-26 23:56:37 -0400

    Remove dotenv from `drizzle.config.ts` (#27)
    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "drizzle-kit";
-import dotenv from "dotenv";
 
-dotenv.config({
-  path: ".env",
-});
+import { env } from "~/env";
 
 export default defineConfig({
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL ?? "",
+    url: env.DATABASE_URL,
   },
   tablesFilter: ["hackwestern2024_*"],
 });
