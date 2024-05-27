a6145b02c0d11a90b95785806061f401c50aa358
Hunter Chen <hello@hunterchen.ca>
2024-05-26 23:36:15 -0400

    go to sleep

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: "",
+    url: process.env.DATABASE_URL ?? "",
   },
   tablesFilter: ["hackwestern2024_*"],
 });
