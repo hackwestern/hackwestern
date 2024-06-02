3d20b481467902d38abc9c744c47fc13fe9c602b
Ben Asokanthan <70033855+basokant@users.noreply.github.com>
2024-06-02 19:02:03 -0400

    Create Preregistration Mutation (#42)
    * add create preregistration mutation
    
    * add create preregistration tests
    
    * remove linter warnings

---
+++ src/server/api/routers/preregistration.test.ts
@@ -0,0 +1,41 @@
+import { mockSession } from "~/server/auth";
+import { db } from "~/server/db";
+import { createInnerTRPCContext } from "../trpc";
+import { createCaller } from "../root";
+import { assert, beforeEach, describe, expect, test } from "vitest";
+import { preregistrations } from "~/server/db/schema";
+import { eq } from "drizzle-orm";
+import { PreregistrationSeeder } from "~/server/db/seed/preregistrationSeeder";
+
+const session = await mockSession(db);
+
+const ctx = createInnerTRPCContext({ session, db });
+const caller = createCaller(ctx);
+
+const testPreregistration = new PreregistrationSeeder().createRandom();
+
+describe("preregistration.create", async () => {
+  beforeEach(async () => {
+    await db
+      .delete(preregistrations)
+      .where(eq(preregistrations.email, testPreregistration.email));
+  });
+
+  test("creates a new preregistration when it does not exist", async () => {
+    const want = testPreregistration;
+    const result = await caller.preregistration.create(want);
+
+    assert(!!result);
+    const { id, createdAt, ...got } = result;
+    void id, createdAt;
+
+    expect(got).toEqual(want);
+  });
+
+  test("throws an error if the preregistration already exists", async () => {
+    await caller.preregistration.create(testPreregistration);
+    await expect(
+      caller.preregistration.create(testPreregistration),
+    ).rejects.toThrowError();
+  });
+});
