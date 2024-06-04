f8f688dc5ee10a040f1dd6f8551817d5eacc3bb3
Hunter <hello@hunterchen.ca>
2024-06-03 23:13:10 -0400

    linting fixes (#47)
    * linting fixes

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -52,8 +52,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        // these might use props but they're not used currently
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
