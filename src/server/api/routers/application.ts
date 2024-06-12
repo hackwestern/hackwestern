ba2ad6638f1b4da6ffc593ac7cca151d86dafafc
basokant <basokanthan@gmail.com>
2024-06-12 14:50:24 -0400

    fix tests

--- src/server/api/routers/application.ts
+++ src/server/api/routers/application.ts
@@ -5,35 +5,41 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { applications } from "~/server/db/schema";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
-import { check } from "prettier";
 
 // Save schema
-const applicationSaveSchema = createInsertSchema(applications)
-  .omit({
-    createdAt: true,
-    updatedAt: true,
-    status: true,
-  })
-  .required({ userId: true });
+export const applicationSaveSchema = createInsertSchema(applications).omit({
+  createdAt: true,
+  updatedAt: true,
+  status: true,
+  userId: true,
+});
 
 // Helper function to check word count within a range
 const checkWordCount = (value: string, min: number, max: number) => {
   const words = value.split(" ");
   return words.length < max && words.length > min;
-}
+};
 
 // Submission schema with data validation
-const applicationSubmitSchema = createInsertSchema(applications, {
+export const applicationSubmitSchema = createInsertSchema(applications, {
   agreeCodeOfConduct: z.literal(true),
   agreeShareWithMLH: z.literal(true),
   agreeWillBe18: z.literal(true),
   age: (schema) => schema.age.min(18),
   resumeLink: (schema) => schema.resumeLink.url(),
   otherLink: (schema) => schema.otherLink.url(),
-  question1: (schema) => schema.question1.refine((value) => checkWordCount(value, 30, 150)),
-  question2: (schema) => schema.question2.refine((value) => checkWordCount(value, 30, 150)),
-  question3: (schema) => schema.question3.refine((value) => checkWordCount(value, 30, 150)),
-})
+  question1: (schema) =>
+    schema.question1.refine((value) => checkWordCount(value, 30, 150)),
+  question2: (schema) =>
+    schema.question2.refine((value) => checkWordCount(value, 30, 150)),
+  question3: (schema) =>
+    schema.question3.refine((value) => checkWordCount(value, 30, 150)),
+}).omit({
+  createdAt: true,
+  updatedAt: true,
+  status: true,
+  userId: true,
+});
 
 export const applicationRouter = createTRPCRouter({
   get: protectedProcedure.query(async ({ ctx }) => {
@@ -70,20 +76,27 @@ export const applicationRouter = createTRPCRouter({
       try {
         const userId = ctx.session.user.id;
         const applicationData = input;
-        const isCompleteApplication = applicationSubmitSchema.safeParse(applicationData).success;
+        const isCompleteApplication =
+          applicationSubmitSchema.safeParse(applicationData).success;
 
         const savedApplication = await ctx.db
           .insert(applications)
-          .values({ 
-            ...applicationData, 
+          .values({
+            ...applicationData,
             githubLink: `https://github.com/${applicationData.githubLink}`,
             linkedInLink: `https://linkedin.com/in/${applicationData.linkedInLink}`,
-            userId, 
-            status: isCompleteApplication ? "PENDING_REVIEW" : "IN_PROGRESS"
+            userId,
+            status: isCompleteApplication ? "PENDING_REVIEW" : "IN_PROGRESS",
           })
           .onConflictDoUpdate({
             target: applications.userId,
-            set: { ...applicationData, updatedAt: new Date(), status: isCompleteApplication ? "PENDING_REVIEW" : "IN_PROGRESS"},
+            set: {
+              ...applicationData,
+              updatedAt: new Date(),
+              githubLink: `https://github.com/${applicationData.githubLink}`,
+              linkedInLink: `https://linkedin.com/in/${applicationData.linkedInLink}`,
+              status: isCompleteApplication ? "PENDING_REVIEW" : "IN_PROGRESS",
+            },
           })
           .returning();
         return savedApplication[0];
