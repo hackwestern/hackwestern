f8f3f4cea24761cb675c99f7f9d47f9cfe0566ea
Joy <joyz030707@gmail.com>
2024-06-12 18:03:44 -0700

    remove error specification

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -136,14 +136,6 @@ export const authRouter = createTRPCRouter({
           success: true,
         };
       } catch (error) {
-        if (error instanceof z.ZodError) {
-          throw new TRPCError({
-            code: 'BAD_REQUEST',
-            message: 'Input validation error',
-            cause: error,
-          });
-        }
-
         throw error instanceof TRPCError
           ? error
           : new TRPCError({ 
