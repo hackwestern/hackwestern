1e0ebefc4d52e8f4402df8a1b1d6f26edbb45c4a
Hunter Chen <kchen762@uwo.ca>
2024-06-20 07:45:45 -0400

    Update auth.ts

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import { randomBytes } from "crypto";
 import { db } from "~/server/db";
 import { mailjet } from "~/server/mail";
