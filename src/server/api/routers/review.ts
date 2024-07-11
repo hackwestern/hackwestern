bfb4f4da27ab17df8bc4f7c38b88c819f3381a3d
LordExodius <oscar45697@gmail.com>
2024-07-11 01:05:47 -0400

    consolidate filters into single query to fetch first reviewable application

--- src/server/api/routers/review.ts
+++ src/server/api/routers/review.ts
@@ -6,6 +6,7 @@ import { z } from "zod";
 import { asc, eq, sql } from "drizzle-orm"
 
 import { db } from "~/server/db";
+import { count } from "console";
 
 const REQUIRED_REVIEWS = 2;
 const REVIEW_TIMEOUT = 24;
@@ -19,58 +20,50 @@ export const reviewRequestRouter = createTRPCRouter({
             await db
                 .update(applications)
                 .set({ status: "PENDING_REVIEW" })
-                .where(sql`${applications.status}='in_review' and ${applications.updatedAt} < now() - interval '${REVIEW_TIMEOUT} hours'`);
+                .where(sql`${applications.status}='IN_REVIEW' and ${applications.updatedAt} < now() - interval '${REVIEW_TIMEOUT} hours'`);
 
             // Remove expired reviews from the review table
             await db
                 .delete(reviews)
-                .where(sql`${applications.status}='in_review' and ${applications.updatedAt} < now() - interval '${REVIEW_TIMEOUT} hours'`);
+                .where(sql`${applications.status}='IN_REVIEW' and ${applications.updatedAt} < now() - interval '${REVIEW_TIMEOUT} hours'`);
 
-            // Find applications awaiting review
+            // Select first application that has not received the required number of reviews and has not been referred
             const applicationsAwaitingReview = await db
                 .select()
                 .from(applications)
-                .where(sql`${applications.status}='pending_review'`)
+                .where(sql`${applications.status}='PENDING_REVIEW'`)
                 .groupBy(applications.userId)
-                .orderBy(asc(applications.updatedAt));
+                .having(sql`count(${reviews.applicantUserId}) < ${REQUIRED_REVIEWS} and ${reviews.referral} is not true`)
+                .orderBy(asc(applications.updatedAt))
+                .limit(1);
+
+            const appToReview = applicationsAwaitingReview[0];
 
             // If there are no applications awaiting review, return
-            if (applicationsAwaitingReview.length < 1) {
+            if (!appToReview) {
                 throw new TRPCError({
                     code: "NOT_FOUND",
                     message: "There are no applications matching this query."
                 })
             }
 
-            // Create a review for the first application that has not received the required number of reviews
-            for (const app of applicationsAwaitingReview) {
-                const matchingReviews = await db
-                    .select()
-                    .from(reviews)
-                    .where(eq(reviews.applicantUserId, app.userId))
+            // Create a new review
+            const newReview = await db
+            .insert(reviews)
+            .values({
+                reviewerUserId: ctx.session.user.id,
+                applicantUserId: appToReview.userId,
+            })
+            .returning();
+            
+            // Update the application status to in_review
+            await db
+                .update(applications)
+                .set({ status: "IN_REVIEW" })
+                .where(eq(applications.userId, appToReview.userId));
 
-                // If the application has received the required number of reviews, continue
-                if (matchingReviews.length >= REQUIRED_REVIEWS) {
-                    continue;
-                }
-
-                // Create a new review
-                const newReview = await db
-                    .insert(reviews)
-                    .values({
-                        reviewerUserId: ctx.session.user.id,
-                        applicantUserId: app.userId,
-                    })
-                    .returning();
-                
-                // Update the application status to in_review
-                await db
-                    .update(applications)
-                    .set({ status: "IN_REVIEW" })
-                    .where(eq(applications.userId, app.userId));
-
-                return newReview;
-            }
+            return {newReview, appToReview};
+            
         } catch (error) {
             throw new TRPCError({
                 code: "INTERNAL_SERVER_ERROR",
