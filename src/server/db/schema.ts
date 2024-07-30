7dd5309bacd49548bd486c667eaeef0bd0d1092c
Hunter Chen <hello@hunterchen.ca>
2024-07-29 22:46:39 -0600

    fix typo

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -188,7 +188,7 @@ export const reviews = createTable(
     githubBonus: smallint("github_bonus").default(0),
     linkedinBonus: smallint("linkedin_bonus").default(0),
     otherlinkBonus: smallint("otherlink_bonus").default(0),
-    referral: boolean("referall").default(false),
+    referral: boolean("referral").default(false),
   },
   (review) => {
     return {
