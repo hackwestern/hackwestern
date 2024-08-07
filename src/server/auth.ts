7e7724c0e5f0c5975927100d98e8143b43e185ec
Ben Asokanthan <70033855+basokant@users.noreply.github.com>
2024-08-06 22:12:51 -0400

    Add getAllApplicants query (#86)
    * add getAllApplicants query and test, and clean up seeder helper functions

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -19,6 +19,7 @@ import { env } from "~/env";
 import { type Database, db } from "~/server/db";
 import { createTable, users } from "~/server/db/schema";
 import { UserSeeder } from "./db/seed/userSeeder";
+import type { PgInsertValue } from "drizzle-orm/pg-core";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -123,6 +124,19 @@ export const getServerAuthSession = (ctx: {
 
 export async function mockSession(db: Database): Promise<Session> {
   const user = new UserSeeder().createRandom();
+  await db.insert(users).values(user).returning();
+
+  return {
+    user,
+    expires: new Date(Date.now() + 10).toISOString(),
+  };
+}
+
+export async function mockOrganizerSession(db: Database): Promise<Session> {
+  const user = {
+    ...new UserSeeder().createRandom(),
+    type: "organizer",
+  } as const satisfies PgInsertValue<typeof users>;
   await db.insert(users).values(user).returning();
 
   return {
