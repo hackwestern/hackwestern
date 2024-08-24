f2ebb6f60936bd7879519fd8dfdcab0305f78b49
Arsalaan.Ali <Arsalaan.Ali@jdpa.com>
2024-08-24 09:15:07 -0400

    text area

--- src/pages/internal/test-ui.tsx
+++ src/pages/internal/test-ui.tsx
@@ -4,6 +4,7 @@ import GoogleAuthButton from "~/components/hw-design/auth/googleauth-button";
 import HWButton from "~/components/hw-design/hw-button";
 import HWIconButton from "~/components/hw-design/hw-icon-button";
 import HWInput from "~/components/hw-design/hw-input";
+import HWTextArea from "~/components/hw-design/hw-textarea";
 function TestUI() {
   return (
     <div className="flex h-screen items-center justify-center">
@@ -17,6 +18,12 @@ function TestUI() {
           title="Email"
           placeholder="john@doe.com"
           subtitle="Enter your email"
+          variant="primary"
+        />
+        <HWTextArea
+          title="Why are you a failure?"
+          placeholder="Enter answer here"
+          subtitle="0/150 words"
           variant="primary"
         />
       </div>
