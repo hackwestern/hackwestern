816d7be8a831b6775d6cface43115639b0d56caa
LordExodius <oscar45697@gmail.com>
2024-09-05 22:26:58 -0400

    add stamp file

---
+++ src/components/ui/stamp.tsx
@@ -0,0 +1,30 @@
+import * as React from "react"
+import { major } from "~/server/db/schema"
+import { PgEnum } from "drizzle-orm/pg-core"
+import { z } from "zod"
+
+type majorType = z.infer<typeof >;
+
+export function MajorStamp(type: major) {
+    if (type === "Computer Science")
+}
+
+export function HWStamp() {
+
+}
+
+export function SchoolStamp() {
+
+}
+
+export function HackerStamp() {
+
+}
+
+export function SubmittedStamp() {
+
+}
+
+export function LinksStamp() {
+    
+}
