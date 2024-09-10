bc6ec55776426d6d9f28855b53d9758a44069a15
Arsalaan.Ali <Arsalaan.Ali@jdpa.com>
2024-09-10 12:18:30 -0400

    remove old color variants

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,10 +26,6 @@ const config = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         githubbg: "#1b1f23",
-        hwprimary: {
-          400: "#C1ACE5",
-          500: "#976CDF"
-        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
           50: "hsl(var(--primary-50))",
