b75bc4241e403020e873148595da802f96fa21b7
basokant <basokanthan@gmail.com>
2024-09-16 22:08:08 -0400

    fix prop type names

--- src/components/apply/avatar.tsx
+++ src/components/apply/avatar.tsx
@@ -1,35 +1,42 @@
 import React from "react";
 import Image from "next/image";
 
-type AvatarType = {
-  avatar:
-    | "Wildlife Wanderer"
-    | "City Cruiser"
-    | "Foodie Fanatic"
-    | "Beach Bum"
-    | null
-    | undefined;
+type AvatarType =
+  | "Wildlife Wanderer"
+  | "City Cruiser"
+  | "Foodie Fanatic"
+  | "Beach Bum"
+  | null
+  | undefined;
+
+type AvatarProps = {
+  avatar: AvatarType;
 };
 
-const Avatar = ({ avatar }: AvatarType) => {
-  const getAvatarImage = () => {
-    switch (avatar) {
-      case "Wildlife Wanderer":
-        return "/images/wildlifewanderer.svg";
-      case "City Cruiser":
-        return "/images/citycruiser.svg";
-      case "Foodie Fanatic":
-        return "/images/foodiefanatic.svg";
-      case "Beach Bum":
-        return "/images/beachbum.svg";
-      default:
-        return "";
-    }
-  };
+function getAvatarImage(avatar: AvatarType) {
+  switch (avatar) {
+    case "Wildlife Wanderer":
+      return "/images/wildlifewanderer.svg";
+    case "City Cruiser":
+      return "/images/citycruiser.svg";
+    case "Foodie Fanatic":
+      return "/images/foodiefanatic.svg";
+    case "Beach Bum":
+      return "/images/beachbum.svg";
+    default:
+      return "";
+  }
+}
 
+const Avatar = ({ avatar }: AvatarProps) => {
   return (
     <div className="flex flex-col items-center justify-end pr-[18.896px]">
-      <Image src={getAvatarImage()} alt="Avatar" width={200} height={204} />
+      <Image
+        src={getAvatarImage(avatar)}
+        alt="Avatar"
+        width={200}
+        height={204}
+      />
       <div className="absolute left-[2%] top-[47%] -rotate-[8.646deg] transform items-center justify-center gap-[10px] border border-[#DCDFE9] bg-[#F8F5FF] p-[4px_8px]">
         <p className="font-sans text-[13px] font-medium leading-[18px] text-[#976CDF]">
           {avatar}
--- src/components/apply/passport.tsx
+++ src/components/apply/passport.tsx
@@ -2,32 +2,20 @@ import React from "react";
 import { applicationSaveSchema } from "~/schemas/application";
 import Avatar from "./avatar";
 import PassportField from "./passport-field";
-//import MajorStamp from './majorstamp';
-//import HWStamp from './hwstamp';
-//import SchoolStamp from './schoolstamp';
-//import HackerStamp from './hackerstamp';
-//import SubmittedStamp from './submittedstamp';
-//import LinksStamp from './linksstamp';
 
 import { z } from "zod";
 
-type Application = {
-  application: z.infer<typeof applicationSaveSchema>;
+type Application = z.infer<typeof applicationSaveSchema>;
+type PassportProps = {
+  application: Application;
 };
 
-const Passport = ({ application }: Application) => {
+const Passport = ({ application }: PassportProps) => {
   const textStyle = "text-[#8E93A5] font-sans text-xs font-medium leading-5";
 
   return (
     <div className="flex h-[626px] w-[570px] flex-shrink-0 flex-col items-start rounded-[10px] border border-[#E3DBF2]">
-      <div className="flex flex-[1_0_0] items-center self-stretch rounded-t-[10px] border-b border-b-[#E3DBF2] bg-gradient-to-l from-[#EFF3FF] via-[#E5E9FF] to-[#ECE9FF]">
-        {/* <MajorStamp type={}/> */}
-        {/* <HWStamp type={}/> */}
-        {/* <SchoolStamp type={}/> */}
-        {/* <HackerStamp type={}/> */}
-        {/* <SubmittedStamp type={}/> */}
-        {/* <LinksStamp type={}/> */}
-      </div>
+      <div className="flex flex-[1_0_0] items-center self-stretch rounded-t-[10px] border-b border-b-[#E3DBF2] bg-gradient-to-l from-[#EFF3FF] via-[#E5E9FF] to-[#ECE9FF]"></div>
       <div className="flex flex-[1_0_0] items-center justify-center gap-10 self-stretch bg-[#F8F5FF] p-10">
         <Avatar avatar={application.avatar} />
         <div className="flex flex-col gap-2">
