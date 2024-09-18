8de26f79193c93a293dc4afa6fff38554d6b537e
basokant <basokanthan@gmail.com>
2024-09-17 23:58:48 -0400

    change input and textarea styling to match the designs

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -14,7 +14,7 @@ const inputVariants = cva(
       variant: {
         default: "",
         primary:
-          "rounded-md bg-primary-50 px-5 py-2 text-black outline outline-2 outline-primary-300",
+          "rounded-md bg-violet-100 px-3 py-2 text-black border-muted",
       },
     },
   },
