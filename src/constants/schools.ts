249f01d6dc0dea319cfe38ddb418c1462ff29d50
Oscar Yu <oscar45697@gmail.com>
2024-09-17 21:39:02 -0400

    Add stamp components (#121)
    * add stamp file
    
    * add stamps and test page
    
    * remove unused sizes
    
    ---------
    
    Co-authored-by: prettify-hackwestern[bot] <prettify-hackwestern[bot]@users.noreply.github.com>

---
+++ src/constants/schools.ts
@@ -0,0 +1,11 @@
+export const schools = [
+  "Western University",
+  "University of Waterloo",
+  "McMaster University",
+  "York University",
+  "University of Toronto",
+  "University of Toronto Mississauga",
+  "University of Toronto Scarborough",
+  "Wilfrid Laurier University",
+  "Other",
+] as const;
