5bd155427b7d1c5ed5acfa79aba60cab7eff8d97
basokant <basokanthan@gmail.com>
2024-09-18 00:10:05 -0400

    change calendar and accent colours

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -44,7 +44,7 @@ function Calendar({
         ),
         day_range_end: "day-range-end",
         day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+          "bg-primary-600 text-primary-foreground hover:bg-primary-600 hover:text-primary-foreground focus:bg-primary-600 focus:text-primary-foreground",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
           "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
