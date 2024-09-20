e883b88ee1adb263d53c3099af2e41cac9967b57
basokant <basokanthan@gmail.com>
2024-09-19 20:40:19 -0400

    fix eslint issues in debounce function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,11 +5,14 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function debounce<T extends Function>(cb: T, wait = 20) {
+export function debounce<T extends (...args: unknown[]) => void>(
+  cb: T,
+  wait = 20,
+) {
   let h: number | NodeJS.Timeout = 0;
-  let callable = (...args: any) => {
+  const callable = (...args: unknown[]) => {
     clearTimeout(h);
     h = setTimeout(() => cb(...args), wait);
   };
-  return <T>(<any>callable);
+  return callable as T;
 }
