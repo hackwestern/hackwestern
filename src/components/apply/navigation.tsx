3dc5097b38cf7f588c0d6b6d70777f7d0c36a042
basokant <basokanthan@gmail.com>
2024-09-19 21:53:40 -0400

    add last saved text

--- src/components/apply/navigation.tsx
+++ src/components/apply/navigation.tsx
@@ -1,7 +1,12 @@
-import { applySteps, type ApplyStep } from "~/constants/apply";
-import { Button } from "../ui/button";
 import Link from "next/link";
 import React from "react";
+import { useMutationState } from "@tanstack/react-query";
+import { getQueryKey } from "@trpc/react-query";
+import { format } from "date-fns";
+
+import { api } from "~/utils/api";
+import { applySteps, type ApplyStep } from "~/constants/apply";
+import { Button } from "../ui/button";
 
 type ApplyNavigationProps = {
   step: ApplyStep | null;
@@ -28,6 +33,36 @@ function getStepIndex(step: ApplyStep | null): number | null {
   return null;
 }
 
+function formattedDate(lastSaved: Date | null): string | null {
+  if (!lastSaved) {
+    return null;
+  }
+  return format(lastSaved, "MMM d, h:m a");
+}
+
+function LastSavedAt() {
+  const lastSavedAtState = useMutationState({
+    filters: { mutationKey: getQueryKey(api.application.get) },
+    select: (mutation) =>
+      mutation.state.submittedAt !== 0
+        ? new Date(mutation.state.submittedAt)
+        : null,
+  });
+  const formattedLastSavedAt = formattedDate(lastSavedAtState[0] ?? null);
+
+  if (!formattedLastSavedAt) {
+    return <span>Not Saved</span>;
+  }
+
+  return (
+    <span className="text-right text-xs italic text-slate-400">
+      Last Saved:
+      <br />
+      {formattedLastSavedAt}
+    </span>
+  );
+}
+
 export function ApplyNavigation({ step }: ApplyNavigationProps) {
   const stepIndex = React.useMemo(() => getStepIndex(step), [step]);
   const previousStep = getPreviousStep(stepIndex);
@@ -41,8 +76,8 @@ export function ApplyNavigation({ step }: ApplyNavigationProps) {
             <Link href={`/apply?step=${previousStep}`}>Back</Link>
           </Button>
         ))}
-      <div className="ml-auto flex">
-        {/* TODO: add last saved text */}
+      <div className="ml-auto flex items-center gap-3">
+        <LastSavedAt />
         <Button variant="primary" asChild className="w-20">
           {!step || !!nextStep ? (
             <Link href={`/apply?step=${nextStep}`}>Next</Link>
