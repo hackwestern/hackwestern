4bd3595a06b2a6a23f312ea853047f989cc635a8
basokant <basokanthan@gmail.com>
2024-09-19 20:27:37 -0400

    add useAutoSave form hook, and debounce util function

---
+++ src/components/hooks/use-auto-save.ts
@@ -0,0 +1,26 @@
+import { useCallback } from "react";
+import { FieldValues, useWatch, type useForm } from "react-hook-form";
+import { debounce } from "~/lib/utils";
+import useDeepCompareEffect from "use-deep-compare-effect";
+
+export function useAutoSave<TFieldValues extends FieldValues = FieldValues>(
+  context: ReturnType<typeof useForm<TFieldValues>>,
+  onSubmit: (data: TFieldValues) => void,
+) {
+  const watch = useWatch({ control: context.control });
+  const { dirtyFields } = context.formState;
+  const hasDirtyFields = Object.keys(dirtyFields).length > 0;
+
+  const debouncedSave = useCallback(
+    debounce(() => {
+      context.handleSubmit(onSubmit)();
+    }, 500),
+    [],
+  );
+
+  useDeepCompareEffect(() => {
+    if (hasDirtyFields) {
+      debouncedSave();
+    }
+  }, [watch]);
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function debounce<T extends Function>(cb: T, wait = 20) {
+  let h: number | NodeJS.Timeout = 0;
+  let callable = (...args: any) => {
+    clearTimeout(h);
+    h = setTimeout(() => cb(...args), wait);
+  };
+  return <T>(<any>callable);
+}
