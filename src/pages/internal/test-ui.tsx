a3b81b1aa84d70791affede4906b477202ecb4db
basokant <basokanthan@gmail.com>
2024-09-24 23:35:36 -0400

    add RadioButtonItem example in test-ui page

--- src/pages/internal/test-ui.tsx
+++ src/pages/internal/test-ui.tsx
@@ -4,7 +4,23 @@ import GoogleAuthButton from "~/components/auth/googleauth-button";
 import { Button } from "~/components/ui/button";
 import { DatePicker } from "~/components/ui/date-picker";
 import { Input } from "~/components/ui/input";
+import { RadioButtonItem, RadioGroup } from "~/components/ui/radio-group";
 import { Textarea } from "~/components/ui/textarea";
+
+const radioItems = [
+  {
+    value: "yes",
+    label: "Yes",
+  },
+  {
+    value: "no",
+    label: "No",
+  },
+  {
+    value: "maybe",
+    label: "Maybe",
+  },
+];
 
 function TestUI() {
   return (
@@ -18,6 +34,11 @@ function TestUI() {
         <Input placeholder="john@doe.com" variant="primary" />
         <Textarea placeholder="Enter answer here" variant="primary" />
         <DatePicker />
+        <RadioGroup className="flex items-center gap-4">
+          {radioItems.map((item) => (
+            <RadioButtonItem key={item.label} {...item} />
+          ))}
+        </RadioGroup>
       </div>
     </div>
   );
