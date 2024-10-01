5710b65b78d33c5c0af7dbc3a7281d85ab021c02
Hunter Chen <hello@hunterchen.ca>
2024-09-30 23:51:00 -0400

    add drawer

--- src/components/apply/menu.tsx
+++ src/components/apply/menu.tsx
@@ -4,14 +4,22 @@ import Link from "next/link";
 import { useMemo, useState } from "react";
 import { getNextStep, getPreviousStep, getStepIndex } from "./navigation";
 import { useRouter } from "next/router";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../ui/drawer";
 
 type ApplyMenuProps = {
   step: ApplyStep | null;
 };
 
 export function ApplyMenu({ step }: ApplyMenuProps) {
-  const [menuVisible, setMenuVisible] = useState(true);
-
   const stepName = step && step?.charAt(0).toUpperCase() + step?.slice(1);
   const isMobile = typeof window !== "undefined" && window.innerWidth < 640;
 
@@ -25,7 +33,25 @@ export function ApplyMenu({ step }: ApplyMenuProps) {
     <div
       className={`fixed bottom-0 mx-auto h-fit w-screen justify-center gap-3 border-[1px] border-t-primary-300 bg-violet-100 py-2 sm:static`}
     >
-      {(menuVisible || !isMobile) && (
+      {isMobile && (
+        <Drawer>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+              <DrawerDescription>
+                This action cannot be undone.
+              </DrawerDescription>
+            </DrawerHeader>
+            <DrawerFooter>
+              <Button>Submit</Button>
+              <DrawerClose>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      )}
+      {!isMobile && (
         <div className="mx-auto flex w-fit flex-col sm:flex-row">
           {applySteps.map((s) => (
             <Button
@@ -61,25 +87,46 @@ export function ApplyMenu({ step }: ApplyMenuProps) {
           />
         </svg>
         <div className="flex gap-2">
+          <Drawer>
+            <DrawerTrigger>
+              <div className={`rounded-2xl bg-primary-300 p-2.5`}>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4 18C3.44772 18 3 17.5523 3 17C3 16.4477 3.44772 16 4 16H20C20.5523 16 21 16.4477 21 17C21 17.5523 20.5523 18 20 18H4ZM4 13C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H4ZM4 8C3.44772 8 3 7.55228 3 7C3 6.44772 3.44772 6 4 6H20C20.5523 6 21 6.44772 21 7C21 7.55228 20.5523 8 20 8H4Z"
+                    fill="#6D3EBA"
+                  />
+                </svg>
+              </div>
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerFooter>
+                {applySteps.map((s) => (
+                  <DrawerClose key={s.step}>
+                    <Button
+                      className="w-full sm:w-auto"
+                      variant={s.step === step ? "apply" : "apply-ghost"}
+                      asChild
+                    >
+                      <Link
+                        href={{ pathname: "/apply", query: { step: s.step } }}
+                      >
+                        {s.label}
+                      </Link>
+                    </Button>
+                  </DrawerClose>
+                ))}
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+
           <div
-            className={`rounded-2xl ${menuVisible && "border border-primary-600"} bg-primary-300 p-2.5`}
-            onClick={() => setMenuVisible(!menuVisible)}
-          >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M4 18C3.44772 18 3 17.5523 3 17C3 16.4477 3.44772 16 4 16H20C20.5523 16 21 16.4477 21 17C21 17.5523 20.5523 18 20 18H4ZM4 13C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H4ZM4 8C3.44772 8 3 7.55228 3 7C3 6.44772 3.44772 6 4 6H20C20.5523 6 21 6.44772 21 7C21 7.55228 20.5523 8 20 8H4Z"
-                fill="#6D3EBA"
-              />
-            </svg>
-          </div>
-          <div
-            className={`rounded-2xl ${menuVisible && "border border-primary-600"} bg-primary-300 p-2.5 font-semibold text-primary-600`}
+            className={`rounded-2xl bg-primary-300 p-2.5 font-semibold text-primary-600`}
           >
             {stepName}
           </div>
