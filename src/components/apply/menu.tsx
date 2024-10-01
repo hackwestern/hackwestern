d2f5e33592e0f11e519341d30f3734b0d609a65a
Hunter Chen <hello@hunterchen.ca>
2024-09-30 23:19:15 -0400

    kinda works

--- src/components/apply/menu.tsx
+++ src/components/apply/menu.tsx
@@ -1,26 +1,90 @@
 import { applySteps, type ApplyStep } from "~/constants/apply";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { useState } from "react";
 
 type ApplyMenuProps = {
   step: ApplyStep | null;
 };
 
 export function ApplyMenu({ step }: ApplyMenuProps) {
+  const [menuVisible, setMenuVisible] = useState(true);
+
+  const stepName = step && step?.charAt(0).toUpperCase() + step?.slice(1);
+  const isMobile = typeof window !== "undefined" && window.innerWidth < 640;
+
   return (
-    // TODO: make this responsive
-    <div className="flex w-full justify-center gap-3 border-[1px] border-t-primary-300 bg-violet-100 py-2">
-      {applySteps.map((s) => (
-        <Button
-          key={s.step}
-          variant={s.step === step ? "apply" : "apply-ghost"}
-          asChild
+    <div
+      className={`fixed bottom-0 mx-auto h-fit w-screen justify-center gap-3 border-[1px] border-t-primary-300 bg-violet-100 py-2 sm:static`}
+    >
+      {(menuVisible || !isMobile) && (
+        <div className="mx-auto flex w-fit flex-col sm:flex-row">
+          {applySteps.map((s) => (
+            <Button
+              className="w-screen sm:w-auto"
+              key={s.step}
+              variant={s.step === step ? "apply" : "apply-ghost"}
+              asChild
+            >
+              <Link href={{ pathname: "/apply", query: { step: s.step } }}>
+                {s.label}
+              </Link>
+            </Button>
+          ))}
+        </div>
+      )}
+      <div className="mx-4 flex h-auto justify-between overflow-clip sm:invisible sm:h-0">
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className=""
         >
-          <Link href={{ pathname: "/apply", query: { step: s.step } }}>
-            {s.label}
-          </Link>
-        </Button>
-      ))}
+          <path
+            d="M3.8284 9.0001L9.1924 14.3641L7.7782 15.7783L6.79994e-07 8.0001L7.7782 0.221999L9.1924 1.6362L3.8284 7.0001L16 7.0001L16 9.0001L3.8284 9.0001Z"
+            fill="#6D3EBA"
+          />
+        </svg>
+        <div className="flex gap-2">
+          <div
+            className={`rounded-2xl ${menuVisible && "border border-primary-600"} bg-primary-300 p-2.5`}
+            onClick={() => setMenuVisible(!menuVisible)}
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4 18C3.44772 18 3 17.5523 3 17C3 16.4477 3.44772 16 4 16H20C20.5523 16 21 16.4477 21 17C21 17.5523 20.5523 18 20 18H4ZM4 13C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H4ZM4 8C3.44772 8 3 7.55228 3 7C3 6.44772 3.44772 6 4 6H20C20.5523 6 21 6.44772 21 7C21 7.55228 20.5523 8 20 8H4Z"
+                fill="#6D3EBA"
+              />
+            </svg>
+          </div>
+          <div
+            className={`rounded-2xl ${menuVisible && "border border-primary-600"} bg-primary-300 p-2.5 font-semibold text-primary-600`}
+          >
+            {stepName}
+          </div>
+        </div>
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className=""
+        >
+          <path
+            d="M12.1716 6.9999L6.8076 1.63589L8.2218 0.22168L16 7.9999L8.2218 15.778L6.8076 14.3638L12.1716 8.9999H0V6.9999H12.1716Z"
+            fill="#6D3EBA"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import { useSearchParams } from "next/navigation";
-
 import { type ApplyStepFull, applySteps } from "~/constants/apply";
 import { ApplyMenu } from "~/components/apply/menu";
 import { ApplyNavbar } from "~/components/apply/navbar";
@@ -20,9 +19,20 @@ export default function Apply() {
     [searchParams],
   );
 
+  const [formVisible, setFormVisible] = useState(true);
+  const [passportVisible, setPassportVisible] = useState(true);
+
   const step = applyStep?.step ?? null;
   const heading = applyStep?.heading ?? null;
   const subheading = applyStep?.subheading ?? null;
+
+  // screen width smaller than sm
+  const isMobile = typeof window !== "undefined" && window.innerWidth < 640;
+
+  if (isMobile && formVisible === passportVisible) {
+    setFormVisible(true);
+    setPassportVisible(false);
+  }
 
   return (
     <>
@@ -36,28 +46,55 @@ export default function Apply() {
       </Head>
       <main className="flex h-svh max-h-svh flex-col items-center bg-primary-50">
         <ApplyNavbar />
+        {isMobile && (
+          <div className="flex w-screen justify-around border-b px-3 md:invisible">
+            <div
+              className={`w-1/2 py-4 text-center ${formVisible ? "border-b border-primary-600 text-primary-600" : "border-b border-slate-400 text-slate-400"}`}
+              onClick={() => {
+                setFormVisible(true);
+                setPassportVisible(false);
+              }}
+            >
+              Form
+            </div>
+            <div
+              className={`w-1/2 py-4 text-center ${passportVisible ? "border-b border-primary-600 text-primary-600" : "border-b border-slate-400 text-slate-400"}`}
+              onClick={() => {
+                setFormVisible(false);
+                setPassportVisible(true);
+              }}
+            >
+              Passport
+            </div>
+          </div>
+        )}
         <div className="flex w-full flex-1 flex-grow items-center">
           {/* TODO: make this responsive */}
-          <div
-            id="left-panel"
-            className="lg:w-xl flex h-full flex-grow flex-col space-y-8 bg-primary-100 p-9 pt-12 lg:max-w-xl"
-          >
-            <div className="space-y-2">
-              <h1 className="text-2xl font-medium">{heading}</h1>
-              <h2 className="text-sm text-slate-500">{subheading}</h2>
+          {formVisible && (
+            <div
+              id="left-panel"
+              className="lg:w-xl flex h-full flex-grow flex-col space-y-8 bg-primary-100 p-9 pt-12 lg:max-w-xl"
+            >
+              <div className="space-y-2">
+                <h1 className="text-2xl font-medium">{heading}</h1>
+                <h2 className="text-sm text-slate-500">{subheading}</h2>
+              </div>
+              <div className="flex-1">
+                <ApplyForm step={step} />
+              </div>
+              <ApplyNavigation step={step} />
             </div>
-            <div className="flex-1">
-              <ApplyForm step={step} />
+          )}
+          {passportVisible && (
+            <div
+              id="right-panel"
+              className="flex h-full w-[60vw] flex-col items-center justify-center"
+            >
+              <Passport />
             </div>
-            <ApplyNavigation step={step} />
-          </div>
-          <div
-            id="right-panel"
-            className="flex h-full w-[60vw] flex-col items-center justify-center"
-          >
-            <Passport />
-          </div>
+          )}
         </div>
+        {isMobile && <div className="py-4">this is a secret</div>}
         <ApplyMenu step={step} />
       </main>
     </>
