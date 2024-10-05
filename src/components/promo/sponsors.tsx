9de10b79a485b451cfb084c596d6aa6dad661a2a
LordExodius <oscar45697@gmail.com>
2024-10-05 02:00:57 -0400

    add sponsor logos

--- src/components/promo/sponsors.tsx
+++ src/components/promo/sponsors.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { Button } from "../ui/button";
+import Image from "next/image";
 
 interface Sponsor {
   name: string;
@@ -8,8 +9,14 @@ interface Sponsor {
 
 const SponsorCard = ({ name, logo }: Sponsor) => {
   return (
-    <div className="h-36 w-full cursor-pointer rounded-xl bg-white transition-all hover:scale-[1.0075] hover:shadow-2xl xl:h-40 2xl:h-48">
-      {name + logo}
+    <div className="p-4 bg-primary-700 flex mx-auto justify-center align-middle h-36 w-full cursor-pointer rounded-xl transition-all hover:scale-[1.0075] xl:h-40 2xl:h-48">
+      <Image
+        src={logo}
+        alt={name}
+        width={0}
+        height={0}
+        className="h-auto w-full"
+      ></Image>
     </div>
   );
 };
@@ -22,27 +29,45 @@ const Sponsors = () => {
       <h1 className="font-MagicRetro text-5xl text-primary-100">
         thank you to our sponsors
       </h1>
-      <div className="font-dmsans py-12 text-xl text-primary-100">
+      <div className="font-dmsans py-12 text-xl text-primary-100 p-5">
         We&apos;d like to extend our appreciation to all of our sponsors that
         help make Hack Western a reality.
       </div>
-      <div className="mx-auto flex w-5/6 flex-col gap-8 py-12 lg:w-3/5 2xl:w-3/5 3xl:w-1/3">
-        {/* Large Sponsors */}
+      <div className="3xl:w-1/3 mx-auto flex w-5/6 flex-col gap-8 py-12 lg:w-3/5 2xl:w-3/5">
+        {/* Title Sponsors */}
+        <h1 className="font-DM_Sans font-bold text-3xl text-primary-100">Title Sponsor</h1>
         <div className="flex columns-1 flex-col">
-          <SponsorCard name="" logo="" />
+          <SponsorCard name="Scotiabank" logo="/sponsors/Scotiabank.svg" />
         </div>
-        {/* Medium Sponsors */}
-        <div className="flex columns-2 flex-col gap-8 lg:flex-row">
-          <SponsorCard name="" logo="" />
-          <SponsorCard name="" logo="" />
+        {/* Diamond Sponsors */}
+        <h1 className="font-DM_Sans font-bold text-3xl text-primary-100">Diamond Sponsors</h1>
+        <div className="columns-1 gap-8 lg:columns-2">
+          <SponsorCard name="Canada Life" logo="/sponsors/CanadaLife.svg" />
+          <div className="lg:hidden h-8" />
+          <SponsorCard name="Sun Life" logo="/sponsors/Sun Life.svg" />
         </div>
-        {/* Small Sponsors */}
-        <div className="flex columns-3 flex-col gap-8 lg:flex-row">
-          <SponsorCard name="" logo="" />
-          <SponsorCard name="" logo="" />
-          <SponsorCard name="" logo="" />
+        <SponsorCard name="Starknet" logo="/sponsors/Starknet.svg" />
+        {/* Gold Sponsors */}
+        <h1 className="font-DM_Sans font-bold text-3xl text-primary-100">Gold Sponsors</h1>
+        <div className="columns-3 flex-col gap-2 lg:gap-8 lg:flex-row">
+          <SponsorCard name="TD invent" logo="/sponsors/TD Invent.svg" />
+          <SponsorCard name="DoraHacks" logo="/sponsors/DoraHacks.svg" />
+          <SponsorCard name="Big Blue Bubble" logo="/sponsors/Big Blue Bubble.svg" />
         </div>
-        <div className="mt-20 flex h-64 flex-col justify-around rounded-xl border border-2 border-[#B07ACA] bg-gradient-to-tr from-[#7B3FDF] via-[#DE81A8] to-[#FFBD80] py-2.5">
+        {/* Bronze Sponsors */}
+        <h1 className="font-DM_Sans font-bold text-3xl text-primary-100">Bronze Sponsors</h1>
+        <div className="columns-3 flex-col gap-2 lg:gap-8 lg:flex-row">
+          <SponsorCard name="P&G" logo="/sponsors/P&G.svg" />
+          <SponsorCard name="Accenture" logo="/sponsors/Accenture.svg" />
+          <SponsorCard name="Morrissette" logo="/sponsors/Morrissette.svg" />
+        </div>
+        {/* In-kind Sponsors */}
+        <h1 className="font-DM_Sans font-bold text-3xl text-primary-100">In-kind Sponsors</h1>
+        <div className="columns-2 flex-col gap-2 lg:gap-8 lg:flex-row">
+          <SponsorCard name="Canada Learning Code" logo="/sponsors/Canada Learning Code.svg" />
+          <SponsorCard name="Warp" logo="/sponsors/Warp.svg" />
+        </div>
+        <div className="mt-20 p-5 flex h-64 flex-col justify-around rounded-xl border border-2 border-[#B07ACA] bg-gradient-to-tr from-[#7B3FDF] via-[#DE81A8] to-[#FFBD80] py-2.5">
           <div className="text-3xl font-bold text-white">
             Help us set some ideas ablaze.
           </div>
