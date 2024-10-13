8f6d7ca512cddf6af9a13f43e935c49140a99601
Arsalaan Ali <38818326+ArsalaanAli@users.noreply.github.com>
2024-10-13 16:46:27 -0400

    App Stats Page for organizer (#205)
    * stats page
    
    * Prettify code
    
    * change redirect to login
    
    ---------
    
    Co-authored-by: prettify-hackwestern[bot] <prettify-hackwestern[bot]@users.noreply.github.com>

--- src/server/api/routers/application.ts
+++ src/server/api/routers/application.ts
@@ -8,7 +8,7 @@ import {
   applicationSubmitSchema,
 } from "~/schemas/application";
 import { GITHUB_URL, LINKEDIN_URL } from "~/utils/urls";
-import { eq } from "drizzle-orm";
+import { eq, count } from "drizzle-orm";
 
 export const applicationRouter = createTRPCRouter({
   get: protectedProcedure.query(async ({ ctx }) => {
@@ -117,4 +117,23 @@ export const applicationRouter = createTRPCRouter({
         });
       }
     }),
+
+  getAppStats: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      const applicationStats = await db
+        .select({
+          status: applications.status,
+          count: count(applications.userId),
+        })
+        .from(applications)
+        .groupBy(applications.status);
+
+      return applicationStats;
+    } catch (error) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Failed to fetch application stats: " + JSON.stringify(error),
+      });
+    }
+  }),
 });
