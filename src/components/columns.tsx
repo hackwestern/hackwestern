1ca7bd75c2e1c98e24c0bf4494b1111f52cde3da
LordExodius <oscar45697@gmail.com>
2024-10-16 03:27:59 -0400

    add name, email, email sorting

--- src/components/columns.tsx
+++ src/components/columns.tsx
@@ -1,19 +1,60 @@
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
 import Link from "next/link";
 import { reviews } from "~/server/db/schema";
+import { Button } from "./ui/button";
+
+
+type applicantType = {
+  email: string,
+  id: string,
+  name: string,
+}
 
 export const reviewDashboardColumns: ColumnDef<typeof reviews.$inferSelect>[] = [
     {
       accessorKey: "applicantUserId",
-      header: "user_id",
+      header: "Edit",
       cell: ({ row }) => {
         const userId: string = row.getValue("applicantUserId")
-        return <Link className="underline" href={`./review?applicant=${userId}`}>{userId}</Link>
+        return <Link href={`./review?applicant=${userId}`}>✏️</Link>
+      }
+    },
+    {
+      accessorKey: "applicant",
+      header: "Name",
+      cell: ({ row }) => {
+        const applicant: applicantType = row.getValue("applicant")
+        const name: string = applicant.name
+        return <div>{name}</div>
+      }
+    },
+    {
+      accessorKey: "applicant",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Email
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+      cell: ({ row }) => {
+        const applicant: applicantType = row.getValue("applicant")
+        const email: string = applicant.email
+        return <div>{email}</div>
       }
     },
     {
       accessorKey: "completed",
-      header: "completed",
+      header: "Completed",
+      cell: ({ row }) => {
+        const completed: boolean = row.getValue("completed")
+        return completed ? "✅" : "❌"
+      }
     },
     {
       accessorKey: "question1Rating",
@@ -30,5 +71,9 @@ export const reviewDashboardColumns: ColumnDef<typeof reviews.$inferSelect>[] = 
     {
       accessorKey: "referral",
       header: "Referral",
+      cell: ({ row }) => {
+        const completed: boolean = row.getValue("referral")
+        return completed ? "🤝" : ""
+      }
     },
   ];
--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -1,9 +1,14 @@
+import React from "react";
+
 import {
   ColumnDef,
+  SortingState,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+
 
 import {
   Table,
@@ -14,6 +19,8 @@ import {
   TableRow,
 } from "~/components/ui/table";
 
+import { Button } from "./ui/button";
+
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
@@ -23,10 +30,17 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+
+    state: {
+      sorting,
+    },
   });
 
   return (
