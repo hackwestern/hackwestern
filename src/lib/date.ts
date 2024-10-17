940f5f1747b70bc2ec50e60da4efac995b709032
basokant <basokanthan@gmail.com>
2024-10-17 00:08:42 -0400

    fix deadline

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,5 +1,5 @@
-export const APPLICATION_DEADLINE = new Date("2024-10-17T00:15:00-05:00");
+export const APPLICATION_DEADLINE = new Date("2024-10-17T00:15:00-04:00");
 
 export function isPastDeadline() {
-  return Date.now().valueOf() >= APPLICATION_DEADLINE.valueOf();
+  return Date.now() >= APPLICATION_DEADLINE.getTime();
 }
