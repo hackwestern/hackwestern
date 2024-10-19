aba06d68fae65c95894406d9aadcdf63751d4220
Hunter Chen <hello@hunterchen.ca>
2024-10-19 01:38:58 -0400

    remove unused pages

--- src/pages/internal/index.tsx
+++ src/pages/internal/index.tsx
@@ -1,87 +1,12 @@
-import Link from "next/link";
-import { Button } from "~/components/ui/button";
-import type { GetServerSidePropsContext } from "next";
-import { getServerSession } from "next-auth";
-import { db } from "~/server/db";
-import { authOptions } from "~/server/auth";
-import type { users } from "~/server/db/schema";
-import NotAuthorizedCard from "~/components/notauthorized-card";
-import { api } from "~/utils/api";
-const Internal = ({
-  userSession,
-}: {
-  userSession: typeof users.$inferSelect;
-}) => {
-  const { data: nextReviewId } = api.review.getNextId.useQuery({});
-
-  return userSession.type == "organizer" ? (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[#160524]">
-      <h1 className="mb-5 text-3xl text-white">Internal Dashboard</h1>
-      <div className="flex flex-col gap-3">
-        <Button
-          asChild
-          className="rounded bg-white p-1 text-center text-black hover:bg-gray-300"
-        >
-          <Link href={`/internal/review?applicant=${nextReviewId}`}>
-            Review Next
-          </Link>
-        </Button>
-        <PreregistrationsButton />
-        <ApplicationsButton />
-      </div>
-    </div>
-  ) : (
-    <NotAuthorizedCard />
-  );
+const Internal = () => {
+  return <></>;
 };
 
-/**
- * Downloads the CSV if authorized as an organizer.
- * @see ./api/application/all.tswsl --set-default-version 2
- */
-function ApplicationsButton() {
-  return (
-    <Link href="/api/application/all?format=csv&mlh">
-      <Button className="w-full rounded bg-white p-1 text-center text-black hover:bg-gray-300">
-        Export Applications
-      </Button>
-    </Link>
-  );
-}
-
-/**
- * Downloads the CSV if authorized as an organizer.
- * @see ./api/preregistration/all.ts
- */
-function PreregistrationsButton() {
-  return (
-    <Link href="/api/preregistration/all?format=csv">
-      <Button className="mx-auto rounded bg-white p-1 text-center text-black hover:bg-gray-300">
-        Export Preregistrations
-      </Button>
-    </Link>
-  );
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context.req, context.res, authOptions);
-  if (!session) {
-    return {
-      redirect: {
-        destination: "internal/login",
-        permanent: false,
-      },
-    };
-  }
-  const user = await db.query.users.findFirst({
-    where: (users, { eq }) => eq(users.id, session.user.id),
-  });
-
+export default Internal;
+export const getServerSideProps = async () => {
   return {
-    props: {
-      userSession: user,
+    redirect: {
+      destination: "/internal/dashboard",
     },
   };
-}
-
-export default Internal;
+};
