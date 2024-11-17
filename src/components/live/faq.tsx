e85c83cd3ae139486787ca44976c7616ac013453
Joy <joyz030707@gmail.com>
2024-11-16 23:05:17 -0500

    live site faq page

--- src/components/live/faq.tsx
+++ src/components/live/faq.tsx
@@ -1,5 +1,34 @@
+import React from "react";
+import { GENERALHACK_FAQ, OTHER_FAQ } from "~/constants/faq";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion";
+
+const FAQ_ITEMS = [...GENERALHACK_FAQ, ...OTHER_FAQ];
+
 const FAQ = () => {
-  return <div>faq</div>;
+  return (
+    <div className="px-10 py-10 md:gap-8">
+      <Accordion
+        type="multiple"
+        className="mx-auto grid gap-x-9 md:grid-cols-2 3xl:w-1/2"
+      >
+        {FAQ_ITEMS.map(({ question, answer }, index) => (
+          <AccordionItem key={index} value={`item-${index}`} className="border-violet-300 my-3">
+            <AccordionTrigger className="text-left text-slate-600">
+              {question}
+            </AccordionTrigger>
+            <AccordionContent className="text-left text-slate-600">
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
 };
 
 export default FAQ;
