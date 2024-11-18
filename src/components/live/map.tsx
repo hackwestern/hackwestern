e95e8115e8f1ab2fe894f8d98ed80dda71f6ed0b
LordExodius <oscar45697@gmail.com>
2024-11-18 17:22:12 -0500

    add map placeholder

--- src/components/live/map.tsx
+++ src/components/live/map.tsx
@@ -1,5 +1,9 @@
 const Map = () => {
-  return <div>map</div>;
+  return <div className="flex w-full h-full p-5 sm:p-10">
+    <div className="bg-primary-300 max-w-4xl w-full h-full rounded-md">
+
+    </div>
+  </div>;
 };
 
 export default Map;
