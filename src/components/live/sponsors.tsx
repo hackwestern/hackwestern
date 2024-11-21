960bda39a228ad47e24503f2709660ddb25a7c92
basokant <basokanthan@gmail.com>
2024-11-20 21:34:27 -0500

    fixed the column issue on the live sponsor page

--- src/components/live/sponsors.tsx
+++ src/components/live/sponsors.tsx
@@ -82,50 +82,38 @@ const Sponsors = () => {
       <h1 className="py-4 font-DM_Sans text-2xl font-medium text-slate-600">
         In-kind Sponsors
       </h1>
-      <div className="flex flex-wrap gap-2 lg:gap-8">
-        <div className="w-1/3">
-          <SponsorCard
-            name="Canada Learning Code"
-            logo="/sponsors/Canada Learning Code.svg"
-            link="https://www.canadalearningcode.ca/"
-          />
-        </div>
-        <div className="w-1/3">
-          <SponsorCard
-            name="Warp"
-            logo="/sponsors/Warp.svg"
-            link="https://www.warp.dev/"
-          />
-        </div>
-        <div className="w-1/3">
-          <SponsorCard
-            name="Voiceflow"
-            logo="/sponsors/Voiceflow.svg"
-            link="https://www.voiceflow.com/"
-          />
-        </div>
-        <div className="w-1/3">
-          <SponsorCard
-            name="StandOut Stickers"
-            logo="/sponsors/StandOut.svg"
-            link="http://hackp.ac/mlh-StandOutStickers-hackathons"
-          />
-        </div>
-        <div className="w-1/3">
-          <SponsorCard
-            name="Western Engineering"
-            logo="/sponsors/WesternEngineering.svg"
-            link="https://www.eng.uwo.ca/outreach/index.html"
-          />
-        </div>
+      <div className="grid grid-cols-3 gap-2 lg:gap-8">
+        <SponsorCard
+          name="Canada Learning Code"
+          logo="/sponsors/Canada Learning Code.svg"
+          link="https://www.canadalearningcode.ca/"
+        />
+        <SponsorCard
+          name="Warp"
+          logo="/sponsors/Warp.svg"
+          link="https://www.warp.dev/"
+        />
+        <SponsorCard
+          name="Voiceflow"
+          logo="/sponsors/Voiceflow.svg"
+          link="https://www.voiceflow.com/"
+        />
+        <SponsorCard
+          name="StandOut Stickers"
+          logo="/sponsors/StandOut.svg"
+          link="http://hackp.ac/mlh-StandOutStickers-hackathons"
+        />
+        <SponsorCard
+          name="Western Engineering"
+          logo="/sponsors/WesternEngineering.svg"
+          link="https://www.eng.uwo.ca/outreach/index.html"
+        />
 
-        <div className="w-1/3">
-          <SponsorCard
-            name="Awake Caffeinated Chocolate"
-            logo="/sponsors/Awake Chocolate.svg"
-            link="https://awakechocolate.com/"
-          />
-        </div>
+        <SponsorCard
+          name="Awake Caffeinated Chocolate"
+          logo="/sponsors/Awake Chocolate.svg"
+          link="https://awakechocolate.com/"
+        />
       </div>
     </div>
   );
