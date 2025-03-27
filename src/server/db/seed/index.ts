d7fa308f90cf5d2572af7ca7cc37ef7a66011e71
Hunter <hello@hunterchen.ca>
2025-03-27 15:50:33 -0400

    Modernize (#286)
    * modernize
    
    * Update pull_request_template.md
    
    * upgrade libraries
    
    * update workflow to use node 23
    
    * Update package-lock.json
    
    * downgrade auth drizzle adapter
    
    * remove cache from github actions (for now)
    
    * Update package-lock.json
    
    * have actions run install
    
    * back to `npm ci`
    
    also package-lock from wsl

--- src/server/db/seed/index.ts
+++ src/server/db/seed/index.ts
@@ -3,6 +3,10 @@ import { seedDatabase } from "./helpers";
 import * as p from "@clack/prompts";
 
 p.intro("Starting hackwestern db seed script");
+if (!process.env.DATABASE_URL?.includes("localhost")) {
+  p.outro("Seed script must only be run on localhost.");
+  process.exit(1);
+}
 try {
   await seedDatabase();
 } catch (e) {
