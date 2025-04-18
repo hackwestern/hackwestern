6f363797246fe71cebe13c62b47a3c0a1df37775
Hunter Chen <hello@hunterchen.ca>
2025-04-18 01:08:28 -0400

    Change `hw11` db prefix to `hw`

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "drizzle-kit";
 import { env } from "~/env";
 
 export default defineConfig({
+  out: "./drizzle",
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-  tablesFilter: ["hw11_*"],
+  tablesFilter: ["hw_*"],
 });
