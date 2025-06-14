4aacf934d05176ec00c58b0c0a323d5122ab08e0
liuwllm <liu.wllm@gmail.com>
2025-06-14 04:05:05 -0400

    Reinstate import that was actually necessary

---
+++ src/components/canvas/reset.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+const Reset = ({
+  onResetViewAndItems
+}: {
+  onResetViewAndItems: () => void
+}) => {
+  return (
+    <div className="text- absolute bottom-4 left-4 z-[1000] flex">
+        <button
+            className="rounded bg-gray-700 p-1.5 font-mono text-xs text-white shadow-md transition-colors hover:bg-gray-600 md:text-sm"
+            onClick={onResetViewAndItems}
+            onPointerDown={(e: React.PointerEvent) => e.stopPropagation()}
+        >
+            <Image src="/images/reset.svg" alt="Reset" width={18} height={18} />
+        </button>
+    </div>
+  );
+};
+
+export default Reset;
