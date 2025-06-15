ecf7f84bb116ccac5d2b6d6df737404079ce1b72
liuwllm <liu.wllm@gmail.com>
2025-06-15 18:03:10 -0400

    Add correct text for submitted state

--- src/components/preregistration-form.tsx
+++ src/components/preregistration-form.tsx
@@ -40,49 +40,47 @@ export function PreregistrationForm({ className }: PreregistrationFormProps) {
   return (
     <div
       className={cn(
-        "flex w-full flex-col items-center justify-center px-12 text-primary",
+        "flex w-full flex-col items-center justify-center px-12 text-primary space-y-4",
         className,
       )}
     >
-      <p className="font-figtree font-medium text-medium">
-        {isSuccess ? "Thanks! You'll hear from us soon." : ""}
-      </p>
-      {!isSuccess && (
-        <Form {...preregistrationForm}>
-          <form
-            className="w-full"
-            onSubmit={preregistrationForm.handleSubmit(onSubmit)}
-          >
-            <FormField
-              control={preregistrationForm.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <div className="flex w-max space-x-2 rounded-xl border border-white bg-white bg-opacity-50 p-1">
-                    <FormLabel className="sr-only">Email Address</FormLabel>
-                    <FormControl>
-                      <div className="py-0.5">
-                        <Input
-                          className="border-none bg-transparent text-heavy"
-                          {...field}
-                          placeholder="jimmy@hacker.ca"
-                        />
-                      </div>
-                    </FormControl>
-                    <div className="pt-1">
-                      <Button variant="primary" className="gap-2">
-                        <span>Submit</span>
-                        <Spinner isLoading={isPending} />
-                      </Button>
+      <Form {...preregistrationForm}>
+        <form
+          className="w-full"
+          onSubmit={preregistrationForm.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={preregistrationForm.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex w-max space-x-2 rounded-xl border border-white bg-white bg-opacity-50 pr-1 pt-1 pb-1">
+                  <FormLabel className="sr-only">Email Address</FormLabel>
+                  <FormControl>
+                    <div className="py-0.5">
+                      <Input
+                        className="border-none bg-transparent text-heavy"
+                        {...field}
+                        placeholder="jimmy@hacker.ca"
+                      />
                     </div>
+                  </FormControl>
+                  <div className="pt-1">
+                    <Button variant="primary" className="gap-2">
+                      <span>Submit</span>
+                      <Spinner isLoading={isPending} />
+                    </Button>
                   </div>
-                  <FormMessage className="text-[#5E28B8]" />
-                </FormItem>
-              )}
-            />
-          </form>
-        </Form>
-      )}
+                </div>
+                <FormMessage className="text-[#5E28B8]" />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+      <p className="font-figtree font-medium text-heavy">
+        {isSuccess ? "Prepare for greatness." : ""}
+      </p>
     </div>
   );
 }
