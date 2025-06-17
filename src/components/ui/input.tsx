d0320f2494d9bda415ca0c88fd2fb25ccd372808
liuwllm <liu.wllm@gmail.com>
2025-06-16 20:23:03 -0400

    Add in missing changes to UI component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -14,6 +14,7 @@ const inputVariants = cva(
       variant: {
         default: "",
         primary: "rounded-md bg-violet-100 px-3 py-2 text-black border-muted",
+        noRing: "focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0",
       },
     },
   },
