9aa1121ceb0b41f795bf587a3e4e0fb3615a6fa7
Hunter Chen <hello@hunterchen.ca>
2025-06-19 04:20:42 -0400

    button cursor-pointer

--- src/components/auth/googleauth-button.tsx
+++ src/components/auth/googleauth-button.tsx
@@ -14,7 +14,7 @@ function GoogleAuthButton({
       onClick={() => {
         void signIn("google", { callbackUrl: redirect });
       }}
-      className="w-full rounded-md bg-slate-50 p-1 font-medium outline outline-1 outline-gray-400 hover:bg-white"
+      className="w-full cursor-pointer rounded-md bg-slate-50 p-1 font-medium outline outline-1 outline-gray-400 hover:bg-white"
     >
       <div className="flex flex-row items-center justify-center gap-1">
         <Image
