715851bcb8caa8893b41701a72034d97a8e82a71
liuwllm <liu.wllm@gmail.com>
2025-06-19 00:35:10 -0400

    Fix z-index issues

--- src/components/promo/Hero.tsx
+++ src/components/promo/Hero.tsx
@@ -7,7 +7,7 @@ function Hero() {
   return (
     <CanvasComponent>
       <div className="my-auto flex flex-col items-center justify-center space-y-12 pb-12">
-        <div>
+        <div className="z-10">
           <Draggable
             key="hw-logo"
             animate={{
