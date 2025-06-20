5c5b9d2594da91e9e75345c78cb647f82770a02f
ayujin <84431267+ayujin@users.noreply.github.com>
2025-06-19 23:36:21 -0400

    toolbar styling

--- src/components/canvas/toolbar.tsx
+++ src/components/canvas/toolbar.tsx
@@ -13,8 +13,8 @@ const Toolbar = ({ zoom, panOffset }: { zoom: number; panOffset: Point }) => {
       className="absolute left-4 top-4 z-[1000] cursor-default rounded bg-white p-2 font-mono text-xs text-heavy shadow-md  md:text-sm"
       onPointerDown={(e: React.PointerEvent) => e.stopPropagation()}
     >
-      x: {(-panOffset.x / zoom - width).toFixed(2)} y:{" "}
-      {(-panOffset.y / zoom - height).toFixed(2)} zoom: {zoom.toFixed(2)}x
+      X {(-panOffset.x / zoom - width).toFixed(0)} Y{" "}
+      {(-panOffset.y / zoom - height).toFixed(0)} {zoom.toFixed(2)}x
     </div>
   );
 };
