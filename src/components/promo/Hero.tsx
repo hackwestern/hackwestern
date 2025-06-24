37b4553d0c5f1b9a2dcf53a7522fb608ed9134db
Rachel Chen <rchen457@uwo.ca>
2025-06-24 00:27:52 -0400

    Update scale and spacing for hero

--- src/components/promo/Hero.tsx
+++ src/components/promo/Hero.tsx
@@ -8,14 +8,14 @@ function Hero() {
   return (
     <CanvasComponent>
       <div className="my-auto flex flex-col items-center justify-center space-y-12 pb-12">
-        <div className="flex origin-center scale-75 flex-col items-center justify-center space-y-12 transition-transform duration-300 ease-in-out md:scale-100">
+        <div className="flex origin-center scale-75 flex-col items-center justify-center space-y-4 transition-transform duration-300 ease-in-out md:scale-100">
           <div className="flex flex-row">
             <Image
               src="/dragme.svg"
               alt="Drag Me!"
               className="pointer-events-none opacity-0"
-              width={100}
-              height={100}
+              width={80}
+              height={80}
             />
             <Draggable
               key="hw-logo"
@@ -32,35 +32,37 @@ function Hero() {
               <motion.img
                 src="/horse.svg"
                 alt="Hack Western Logo"
-                width={100}
-                height={100}
+                width={80}
+                height={80}
                 draggable="false"
-                className="h-auto w-auto scale-75"
+                className="h-auto w-auto scale-[0.65]"
               />
             </Draggable>
             <Image
               src="/dragme.svg"
               alt="Drag Me!"
               className="pointer-events-none"
-              width={100}
-              height={100}
+              width={80}
+              height={80}
             />
           </div>
-          <div className="font-jetbrains-mono text-medium">
-            NOV 21 - NOV 23, 2025
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <motion.img
-              src="/hackwesterntitle.svg"
-              alt="Hack Western XII"
-              width={500}
-              height={100}
-              className="h-[150px] w-auto"
-              draggable="false"
-            />
-          </div>
-          <div className="font-figtree text-sm text-medium">
-            The world is your canvas.
+          <div className="my-auto flex flex-col items-center justify-center space-y-12 pb-6">
+            <div className="font-jetbrains-mono text-medium">
+              NOV 21 - NOV 23, 2025
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <motion.img
+                src="/hackwesterntitle.svg"
+                alt="Hack Western XII"
+                width={500}
+                height={100}
+                className="h-[180px] w-auto"
+                draggable="false"
+              />
+            </div>
+            <div className="text-md font-figtree text-medium">
+              The world is your canvas.
+            </div>
           </div>
           <PreregistrationForm />
         </div>
