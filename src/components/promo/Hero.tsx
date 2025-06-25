be19d19b6dc549aea4999139c7bdea63bf8f0a54
Rachel Chen <85042944+rachelchxn@users.noreply.github.com>
2025-06-25 01:02:17 -0400

    Rachel styling edits (#364)
    * Update scale and spacing for hero
    
    * Navbar styling for consistency
    
    update icons, labels, font, colours, and borders
    
    * Horse sticker interaction enhanced
    
    "drag me" disappears on drag and cursor pointer on hover
    
    * Canvas element styling tweaks
    
    nav, coords/zoom, mlh footer
    
    * Update favicon.ico
    
    * Style updates and colours added
    
    Colour styles, shadow, input translucent effect
    
    * Prettify code
    
    * Email copied in tooltip
    
    * revision changes
    
    ---------
    
    Co-authored-by: prettify-hackwestern[bot] <prettify-hackwestern[bot]@users.noreply.github.com>

--- src/components/promo/Hero.tsx
+++ src/components/promo/Hero.tsx
@@ -3,22 +3,26 @@ import { PreregistrationForm } from "../preregistration-form";
 import { Draggable } from "~/components/canvas/draggable";
 import { CanvasComponent } from "../canvas/canvas";
 import Image from "next/image";
+import { useState } from "react";
 
 function Hero() {
+  const [hasBeenDragged, setHasBeenDragged] = useState(false);
+
   return (
     <CanvasComponent>
       <div className="my-auto flex flex-col items-center justify-center space-y-12 pb-12">
-        <div className="flex origin-center scale-75 flex-col items-center justify-center space-y-12 transition-transform duration-300 ease-in-out md:scale-100">
+        <div className="flex origin-center scale-75 flex-col items-center justify-center space-y-4 transition-transform duration-300 ease-in-out md:scale-100">
           <div className="flex flex-row">
             <Image
               src="/dragme.svg"
               alt="Drag Me!"
-              className="pointer-events-none opacity-0"
-              width={100}
-              height={100}
+              className=" pointer-events-none opacity-0"
+              width={80}
+              height={80}
             />
             <Draggable
               key="hw-logo"
+              onDragStart={() => setHasBeenDragged(true)}
               animate={{
                 rotate: [2, -2],
                 transition: {
@@ -32,35 +36,42 @@ function Hero() {
               <motion.img
                 src="/horse.svg"
                 alt="Hack Western Logo"
-                width={100}
-                height={100}
+                width={80}
+                height={80}
                 draggable="false"
-                className="h-auto w-auto scale-75"
+                className="cursor-drag active:cursor-dragging h-auto w-auto scale-[0.65]"
               />
             </Draggable>
-            <Image
-              src="/dragme.svg"
-              alt="Drag Me!"
-              className="pointer-events-none"
-              width={100}
-              height={100}
-            />
+            <motion.div
+              animate={{ opacity: hasBeenDragged ? 0 : 1 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
+              <Image
+                src="/dragme.svg"
+                alt="Drag Me!"
+                className="pointer-events-none -ml-2 pt-14"
+                width={80}
+                height={80}
+              />
+            </motion.div>
           </div>
-          <div className="font-jetbrains-mono text-medium">
-            NOV 21 - NOV 23, 2025
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <motion.img
-              src="/hackwesterntitle.svg"
-              alt="Hack Western XII"
-              width={500}
-              height={100}
-              className="h-[150px] w-auto"
-              draggable="false"
-            />
-          </div>
-          <div className="font-figtree text-sm text-medium">
-            The world is your canvas.
+          <div className="my-auto flex flex-col items-center justify-center space-y-12 pb-6">
+            <div className="text-md font-jetbrains-mono text-medium">
+              NOV 21 - NOV 23, 2025
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <motion.img
+                src="/hackwesterntitle.svg"
+                alt="Hack Western XII"
+                width={500}
+                height={100}
+                className="h-[180px] w-auto"
+                draggable="false"
+              />
+            </div>
+            <div className="text-md font-figtree text-medium">
+              The world is your canvas.
+            </div>
           </div>
           <PreregistrationForm />
         </div>
