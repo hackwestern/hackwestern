7fb174915d8e3e4f490b7e822f43e9a94ea5d376
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2025-07-10 15:33:23 -0400

    Fixed "Get in touch" button

--- src/components/promo/SponsorSection.tsx
+++ src/components/promo/SponsorSection.tsx
@@ -1,6 +1,9 @@
 import { CanvasComponent } from "../canvas/canvas";
 import { useState } from "react";
+import { motion } from "framer-motion";
 import Image from "next/image";
+import { Button } from "../ui/button";
+import { Mail } from "lucide-react";
 
 function SponsorSection(){
 
@@ -16,13 +19,13 @@ function SponsorSection(){
 
                     <div className="w-[872.27px] h-[626.11px] relative origin-top-left rotate-[-1.77deg] bg-gradient-to-b from-gray-200 to-zinc-400 rounded-3xl shadow-[0px_3.8216559886932373px_7.643311977386475px_0px_rgba(0,0,0,0.25)] shadow-[inset_0px_-1.9108279943466187px_11.464967727661133px_0px_rgba(0,0,0,0.40)]">
             <div className="w-24 h-36 left-[386.49px] top-[239.58px] absolute">
-                <Image
+                <motion.img
                 src="/hackwesternmaclogo.svg"
                 alt="hackwestern"
                 width={95.541}
                 height={144.834}
-                >
-                </Image>
+                draggable="false"
+                />
             </div>
             <div data-property-1="Default" className="w-96 h-28 left-[29.33px] top-[49px] absolute origin-top-left rotate-[1.77deg]">
                 <div className="w-96 h-20 left-[1.13px] top-[16px] absolute">
@@ -30,18 +33,14 @@ function SponsorSection(){
                 </div>
             </div>
         </div>
-
         
-
-        
-
-        <Image
+        <motion.img
         src="/notepad.svg"
         alt="notepad"
         width={400}
-        height={380}>
-            
-        </Image>
+        height={380}
+        draggable="false"
+        />
 
         <div className="w-96 origin-top-left rotate-[7.65deg] inline-flex flex-col justify-start items-center gap-7">
             <div className="self-stretch flex flex-col justify-start items-center gap-2.5">
@@ -50,13 +49,18 @@ function SponsorSection(){
                 </div>
                 <div className="w-96 text-center justify-start text-zinc-500 text-base font-medium font-['Figtree']">Interested in supporting the event?</div>
             </div>
-            <div className="px-4 pt-2.5 pb-3 bg-gradient-to-l from-slate-500 to-purple-400 rounded-lg shadow-[0px_2px_4px_0px_rgba(60,32,76,0.20)] border-l border-r border-t border-b-4 border-black/50 inline-flex justify-center items-center gap-2.5">
                 <div className="w-4 h-4 relative overflow-hidden">
-                    <div className="w-3.5 h-2.5 left-[2.43px] top-[3.57px] absolute outline outline-[1.33px] outline-offset-[-0.67px] outline-white" />
                 </div>
-                <div className="justify-start text-white text-base font-medium font-['Figtree']">Get in touch</div>
+                <Button
+                      variant="primary"
+                      className="w-40 pt-1 pr-2 pl-2 pb-1 gap-1"                      
+                      type="submit"
+                >
+                <Mail className="ml-2 h-6 w-6" />
+                    Get in touch
+                
+                </Button>
             </div>
-        </div>
 
         </CanvasComponent>
     )
