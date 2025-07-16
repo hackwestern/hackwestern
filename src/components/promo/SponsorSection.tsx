0a1516381a4b2deefcbb7c10580edf911f895f62
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2025-07-16 15:49:53 -0400

    Desktop viewport

--- src/components/promo/SponsorSection.tsx
+++ src/components/promo/SponsorSection.tsx
@@ -1,7 +1,5 @@
 import { CanvasComponent } from "../canvas/canvas";
-import { useState } from "react";
 import { motion } from "framer-motion";
-import Image from "next/image";
 import { Button } from "../ui/button";
 import { Mail } from "lucide-react";
 
@@ -11,12 +9,12 @@ function SponsorSection() {
     <div className="my-auto flex flex-col items-center justify-center space-y-12 pb-12">
 
       {/* Our Sponsors Text Section */}
-      <div className="inline-flex w-[794px] flex-col items-center justify-start gap-6">
+      <div className="inline-flex w-[794px] flex-col items-center justify-start gap-8 pb-12">
         <div className="h-5 justify-start self-stretch text-center font-['JetBrains_Mono'] text-base font-medium uppercase text-zinc-500">
           Our sponsors
         </div>
         <div className="flex flex-col items-start justify-start gap-3 self-stretch">
-          <div className="justify-start self-stretch text-center font-['Dico'] text-3xl font-medium text-indigo-950">
+          <div className="justify-start self-stretch text-center font-['Dico'] text-4xl font-medium text-indigo-950">
             Our sponsors make Hack Western possible.
           </div>
           <div className="justify-start self-stretch text-center font-['Figtree'] text-base font-medium text-zinc-500">
@@ -24,9 +22,13 @@ function SponsorSection() {
           </div>
         </div>
       </div>
+
       <div className="flex">
+
         {/* Macbook */}
-        <div className="relative h-[626.11px] w-[872.27px] origin-top-left rotate-[-1.77deg] rounded-3xl bg-gradient-to-b from-gray-200 to-zinc-400 shadow-[0px_3.8216559886932373px_7.643311977386475px_0px_rgba(0,0,0,0.25)] shadow-[inset_0px_-1.9108279943466187px_11.464967727661133px_0px_rgba(0,0,0,0.40)]">
+        <div
+          className="relative origin-top-left rounded-3xl bg-gradient-to-b from-gray-200 to-zinc-400 shadow-[0px_3.8216559886932373px_7.643311977386475px_0px_rgba(0,0,0,0.25)] shadow-[inset_0px_-1.9108279943466187px_11.464967727661133px_0px_rgba(0,0,0,0.40)] z-10 w-[854.14px] h-[600px] -rotate-[1.771deg]"
+        >
           <div className="absolute left-[386.49px] top-[239.58px] h-36 w-24">
             <motion.img
               src="/hackwesternmaclogo.svg"
@@ -45,35 +47,45 @@ function SponsorSection() {
         </div>
 
         {/* Notepad */}
-        <div className="inline-flex w-96 origin-top-left rotate-[7.65deg] flex-col items-center justify-start gap-7">
-          <div className="relative">
+        <div
+          className="origin-top-left flex flex-col items-center w-[400px] h-[380px]"
+        >
+            <div className="relative w-full h-full scale-110">
             <motion.img
               src="/notepad.svg"
               alt="notepad"
-              width={400}
-              height={380}
+              width={440}
+              height={418}
               draggable="false"
             />
-            <div className="absolute inset-0 flex flex-col items-center justify-start pt-16 px-8">
-              <div className="w-96 justify-start text-center font-['JetBrains_Mono'] text-2xl font-medium uppercase text-indigo-950">
-                Sponsor a weekend of inspiration and creation.
+            <div
+              className="absolute inset-0 flex flex-col items-center justify-center gap-[29px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-full w-[450px] px-10 rotate-[7.647deg]"
+            >
+              <div className="space-y-2">
+                 <div
+                  className="w-full text-center text-[#3C204C] font-jetbrains-mono text-xl font-medium tracking-tighter uppercase"
+                >
+                  <span className="inline-block max-w-[350px] break-words">
+                    SPONSOR A WEEKEND OF<br />
+                    INSPIRATION AND CREATION.
+                  </span>
+                </div>
+                <div className="max-w-[320px] break-words w-full justify-center text-center font-figtree text-sm font-medium text-zinc-500">
+                  Interested in supporting the event?
+                </div>
+              </div>
+              <div className="flex w-full justify-center">
+                <Button
+                  variant="primary"
+                  className="w-auto px-6 py-3 gap-2"
+                  type="submit"
+                >
+                  <Mail className="h-5 w-5" />
+                  Get in touch
+                </Button>
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-center justify-start gap-2.5 self-stretch">
-            <div className="w-96 justify-start text-center font-['Figtree'] text-base font-medium text-zinc-500">
-              Interested in supporting the event?
-            </div>
-          </div>
-          {/* <div className="relative h-4 w-4 overflow-hidden"></div> */}
-          <Button
-            variant="primary"
-            className="w-40 gap-1 pb-1 pl-2 pr-2 pt-1"
-            type="submit"
-          >
-            <Mail className="ml-2 h-6 w-6" />
-            Get in touch
-          </Button>
         </div>
       </div>
 
