0a75a3e9b98651cdbc525b1391b4c89a1b4d3b6d
Hunter <hello@hunterchen.ca>
2025-07-21 19:38:14 -0400

    Small FAQ Changes (#389)
    * slight faq shifts for better mobile
    
    * improve faq formatting
    
    * Prettify code
    
    ---------
    
    Co-authored-by: prettify-hackwestern[bot] <prettify-hackwestern[bot]@users.noreply.github.com>

--- src/constants/faq.ts
+++ src/constants/faq.ts
@@ -33,9 +33,9 @@ export const PROMO_FAQ: readonly [
       "Hackers can be in teams of up to 4 people, which means you could also solo hack if you wish. You do not need to tell us who your team members are, simply add them to your Devpost submission before the deadline.",
   },
   {
-    question: "What is a hackathon?",
+    question: "What workshops and activities will there be?",
     answer:
-      "A hackathon is an event designed for students to experiment with tech and bring a project idea to life. At Hack Western, participants will collaborate in teams of up to four to build web, mobile, and hardware projects. Students will get the opportunity to receive mentorship from industry professionals, connect with sponsors and recruiters, and compete for some cool prizes!",
+      "You won't be hacking for 36 hours straight, as there will be many workshops and activities you can participate in during the event as well as the week leading up to it. Last year, our workshops included topics like creating chrome extensions, building voice apps, pitching your hackathon project, recruiting, and more. Our full schedule will be released closer to the event!",
   },
   {
     question: "When can I start hacking?",
@@ -43,14 +43,14 @@ export const PROMO_FAQ: readonly [
       "Hacking begins at 10pm on Friday, November 21st, 2025 and ends at 10am on Sunday, November 23rd, 2025. All code written for Hack Western must be written within this timeframe, with the exception of code taken from public and open source libraries, APIs, or tutorials. Any projects found to be worked on outside of this time frame will be disqualified from judging.",
   },
   {
-    question: "What workshops and activities will there be?",
+    question: "What is a hackathon?",
     answer:
-      "You won't be hacking for 36 hours straight, as there will be many workshops and activities you can participate in during the event as well as the week leading up to it. Last year, our workshops included topics like creating chrome extensions, building voice apps, pitching your hackathon project, recruiting, and more. Our full schedule will be released closer to the event!",
+      "A hackathon is an event designed for students to experiment with tech and bring a project idea to life. At Hack Western, participants will collaborate in teams of up to four to build web, mobile, and hardware projects. Students will get the opportunity to receive mentorship from industry professionals, connect with sponsors and recruiters, and compete for some cool prizes!",
   },
   {
-    question: "How many people can be on my team?",
+    question: "Who can participate?",
     answer:
-      "Your team cannot exceed more than four members. You can also work on a project by yourself, but we believe that hacking is more fun with friends. The more, the merrier!",
+      "Hack Western applications are open to post-secondary students from across the world. Whether you are a freshman or completing your Ph.D., we want you to apply! Unfortunately, we will not be accepting high school students at this time.\nEach application is reviewed from the Hack Western organizing team, and decisions will be sent out on a rolling basis.",
   },
   {
     question: "Can I start working on my hack before the event?",
@@ -63,9 +63,9 @@ export const PROMO_FAQ: readonly [
       "Hack Western is completely free to attend, including free food and swag! Participants travelling to the event outside of Western and provided bus routes however may incur additional costs. ",
   },
   {
-    question: "Who can participate?",
+    question: "How many people can be on my team?",
     answer:
-      "Hack Western applications are open to post-secondary students from across the world. Whether you are a freshman or completing your Ph.D., we want you to apply! Unfortunately, we will not be accepting high school students at this time.\nEach application is reviewed from the Hack Western organizing team, and decisions will be sent out on a rolling basis.",
+      "Your team cannot exceed more than four members. You can also work on a project by yourself, but we believe that hacking is more fun with friends. The more, the merrier!",
   },
   {
     question: "Will there be food and drinks??",
