df2b6009256aa2aab4ddf65f23b3194561be7923
Hunter Chen <hello@hunterchen.ca>
2025-07-22 17:59:15 -0400

    shift everything in canvas a little right

--- src/constants/canvas.ts
+++ src/constants/canvas.ts
@@ -9,32 +9,32 @@ export enum CanvasSection {
 
 export const coordinates = {
   [CanvasSection.About]: {
-    x: 1900,
+    x: 2350,
     y: 550,
     width: 1013,
   },
   [CanvasSection.Projects]: {
-    x: 6100,
+    x: 6550,
     y: 550,
     width: 1200,
   },
   [CanvasSection.Home]: {
-    x: 3400,
+    x: 3850,
     y: 1800,
     width: undefined, // home is screen width
   },
   [CanvasSection.FAQ]: {
-    x: 1350,
+    x: 1800,
     y: 3000,
     width: 1768,
   },
   [CanvasSection.Sponsors]: {
-    x: 4350,
+    x: 4800,
     y: 4100,
     width: 1240,
   },
   [CanvasSection.Team]: {
-    x: 6400,
+    x: 6850,
     y: 3000,
     width: 1080,
   },
