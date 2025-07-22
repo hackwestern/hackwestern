b967d67d655c18283ec30ffdacba0d37dc52a9b1
Hunter <hello@hunterchen.ca>
2025-07-21 20:13:08 -0400

    Promo V2 Barebones Team Page (#386)
    * faq cards
    
    * more font weights
    
    * Prettify code
    
    * Update faq.tsx
    
    * Update faq.tsx
    
    * Prettify code
    
    * Update faq.tsx
    
    * move to own folder
    
    * Prettify code
    
    * Update card.tsx
    
    * partially vibe coded book
    
    * kinda works
    
    * i have schizophrenia
    
    * Prettify code
    
    * minor changes
    
    * close enough
    
    * visual labels
    
    * Prettify code
    
    * label
    
    * better labels
    
    * yay
    
    * Prettify code
    
    * random images
    
    * Prettify code
    
    * non-optional back page
    
    * fixed wierd bounding thing i think
    
    * 0.2 min zoom
    
    * memoize some components
    
    * quicker page flip speed
    
    ---------
    
    Co-authored-by: prettify-hackwestern[bot] <prettify-hackwestern[bot]@users.noreply.github.com>

---
+++ src/components/promo/team/teams.tsx
@@ -0,0 +1,142 @@
+export const PAGES = [
+  {
+    label: "CO-DIRECTORS",
+    labelOffset: 10,
+    front: (
+      <div>
+        Edison Swathi
+        <img src="/horse.svg" alt="aboutcloud" className="mx-auto h-24 w-24" />
+      </div>
+    ),
+    back: (
+      <div>
+        design design
+        <img
+          src="/images/aboutcloud2.svg"
+          alt="aboutcloud2"
+          className="mx-auto h-24 w-24"
+        />
+      </div>
+    ),
+  },
+  {
+    label: "DESIGN",
+    labelOffset: 126,
+    front: (
+      <div>
+        horse horse horse
+        <img
+          src="/images/aboutticket.svg"
+          alt="aboutticket"
+          className="mx-auto h-24 w-24"
+        />
+      </div>
+    ),
+    back: (
+      <div>
+        text text text
+        <img
+          src="/images/beachbum.svg"
+          alt="beachbum"
+          className="mx-auto h-24 w-24"
+        />
+      </div>
+    ),
+  },
+  {
+    label: "EVENTS",
+    labelOffset: 195,
+    front: (
+      <div>
+        events 2
+        <img
+          src="/images/citycruiser.svg"
+          alt="citycruiser"
+          className="mx-auto h-24 w-24"
+        />
+      </div>
+    ),
+    back: (
+      <div>
+        marketing
+        <img
+          src="/images/cloud1.svg"
+          alt="cloud1"
+          className="mx-auto h-24 w-24"
+        />
+      </div>
+    ),
+  },
+  {
+    label: "MARKETING",
+    labelOffset: 264,
+    front: (
+      <div>
+        marketing marketing
+        <img
+          src="/images/cloud2.svg"
+          alt="cloud2"
+          className="mx-auto h-24 w-24"
+        />
+      </div>
+    ),
+    back: (
+      <div>
+        spon
+        <img
+          src="/images/cloud3.svg"
+          alt="cloud3"
+          className="mx-auto h-24 w-24"
+        />
+      </div>
+    ),
+  },
+  {
+    label: "SPONSORSHIP",
+    labelOffset: 357,
+    front: (
+      <div>
+        spon
+        <img
+          src="/images/cloud4.svg"
+          alt="cloud4"
+          className="mx-auto h-24 w-24"
+        />
+      </div>
+    ),
+    back: (
+      <div>
+        web
+        <img
+          src="/images/cloud5.svg"
+          alt="cloud5"
+          className="mx-auto h-24 w-24"
+        />
+      </div>
+    ),
+  },
+  {
+    label: "WEB",
+    labelOffset: 466,
+    front: (
+      <div>
+        web web web
+        <img
+          src="/images/cloud6.svg"
+          alt="cloud6"
+          className="mx-auto h-24 w-24"
+        />
+      </div>
+    ),
+    back: (
+      <div>
+        bonus page
+        <img
+          src="/images/cloud7.svg"
+          alt="cloud7"
+          className="mx-auto h-24 w-24"
+        />
+      </div>
+    ),
+  },
+] as const;
