0b84b833f5080eba5e00d6f5274788b5c99c0f3a
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2025-07-25 12:18:15 -0400

    Refactored Projects Section
    Combined "PastProjects" component into the "projects" component. Also I
    did all of this in NeoVim
    #NoElectronBloatwareWasUsedInTheMakingOfThisCommit

--- src/components/promo/projects.tsx
+++ src/components/promo/projects.tsx
@@ -1,14 +1,35 @@
 import { coordinates } from "~/constants/canvas";
 import { CanvasComponent } from "../canvas/canvas";
+import { motion } from "framer-motion";
 
 function Projects() {
-  return (
-    <CanvasComponent offset={coordinates.projects}>
-      <div className="flex h-screen w-screen items-center justify-center">
-        <h1 className="text-5xl font-bold">Projects</h1>
-      </div>
-    </CanvasComponent>
-  );
+	return (
+		<CanvasComponent offset={coordinates.projects}>
+			<div className="flex h-screen w-screen items-center justify-center">
+				<div className="inline-flex w-[640px] flex-col items-center justify-center gap-6">
+					<div className="justify-start self-stretch text-center font-jetbrains-mono text-base font-medium uppercase text-zinc-500">
+						past projects
+					</div>
+					<div className="justify-start self-stretch text-center font-dico text-3xl font-medium text-indigo-950">
+						The world is waiting for your creation.
+					</div>
+					<div className="justify-start self-stretch text-center font-figtree text-base font-medium text-zinc-500">
+						Here are what other students like you have created at Hack Western.
+					</div>
+					<motion.img
+						src="/PastProjectsBubble.svg"
+						alt="Past Projects Bubbld"
+						width={440}
+						height={418}
+						draggable="false"
+					/>
+					<div className="w-[640px] justify-start text-center font-['Figtree'] text-base font-medium text-zinc-500">
+						Here are what other students like you have created at Hack Western.
+					</div>
+				</div>
+			</div>
+		</CanvasComponent>
+	);
 }
 
 export default Projects;
