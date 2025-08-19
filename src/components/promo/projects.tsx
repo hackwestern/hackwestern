acb7e76eef09bab18b871079f4aac616c500e41a
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2025-08-19 17:41:49 -0400

    removed headercards props

--- src/components/promo/projects.tsx
+++ src/components/promo/projects.tsx
@@ -3,58 +3,51 @@ import { coordinates } from "~/constants/canvas";
 import { useState } from "react";
 import { folderData } from "~/constants/folderData";
 import { ProjectPreview } from "../ui/PreviewProjects";
-import { projectsPlaceHolder } from "~/constants/projectsPlaceholder";
 import HeaderCards from "../ui/HeaderCards";
 
 function Projects() {
-  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-  return (
-    <CanvasComponent offset={coordinates.projects}>
-      <div className="flex h-[815px] w-full shrink-0 flex-col items-center">
-        {/* Top projects */}
-        <HeaderCards
-          names={projectsPlaceHolder[0].map(([image, description]) => ({
-            image,
-            description,
-          }))}
-          folder={projectsPlaceHolder[1]}
-        />
-        <div className="mb-[100px] flex h-[111px] w-full shrink-0 items-center justify-center">
-          <div className="inline-flex w-full shrink-0 flex-col items-center justify-center">
-            <div className="mb-[24px] justify-start self-stretch text-center font-jetbrains-mono text-base font-medium uppercase text-zinc-500">
-              past projects
-            </div>
-            <div className="justify-start self-stretch text-center font-dico text-3xl font-medium text-indigo-950">
-              The world is waiting for your creation.
-            </div>
-            <div className="justify-start self-stretch text-center font-figtree text-base font-medium text-zinc-500">
-              Here are what other students like you have created at Hack
-              Western.
-            </div>
-          </div>
-        </div>
-        {/* Folders */}
-        <div className="flex h-[180px] w-[60rem] shrink-0 justify-between">
-          {folderData.map((folder, index) => (
-            <div
-              key={folder.label}
-              className={`${folder.align} relative flex flex-col items-center`}
-              onMouseEnter={() => setHoveredIndex(index)}
-              onMouseLeave={() => setHoveredIndex(null)}
-            >
-              <ProjectPreview
-                label={folder.label}
-                gradientId={folder.gradientId}
-                isOpen={hoveredIndex === index}
-                cards={folder.cards}
-                folder={folder.label}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-    </CanvasComponent>
-  );
+	const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+	return (
+		<CanvasComponent offset={coordinates.projects}>
+			<div className="flex h-[815px] w-full shrink-0 flex-col items-center">
+				{/* Top projects */}
+				<HeaderCards />
+				<div className="mb-[100px] flex h-[111px] w-full shrink-0 items-center justify-center">
+					<div className="inline-flex w-full shrink-0 flex-col items-center justify-center">
+						<div className="mb-[24px] justify-start self-stretch text-center font-jetbrains-mono text-base font-medium uppercase text-zinc-500">
+							past projects
+						</div>
+						<div className="justify-start self-stretch text-center font-dico text-3xl font-medium text-indigo-950">
+							The world is waiting for your creation.
+						</div>
+						<div className="justify-start self-stretch text-center font-figtree text-base font-medium text-zinc-500">
+							Here are what other students like you have created at Hack
+							Western.
+						</div>
+					</div>
+				</div>
+				{/* Folders */}
+				<div className="flex h-[180px] w-[60rem] shrink-0 justify-between">
+					{folderData.map((folder, index) => (
+						<div
+							key={folder.label}
+							className={`${folder.align} relative flex flex-col items-center`}
+							onMouseEnter={() => setHoveredIndex(index)}
+							onMouseLeave={() => setHoveredIndex(null)}
+						>
+							<ProjectPreview
+								label={folder.label}
+								gradientId={folder.gradientId}
+								isOpen={hoveredIndex === index}
+								cards={folder.cards}
+								folder={folder.label}
+							/>
+						</div>
+					))}
+				</div>
+			</div>
+		</CanvasComponent>
+	);
 }
 
 export default Projects;
