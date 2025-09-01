26fdf4654ed11a0775dc3ce1f7dbc4a01412160e
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2025-09-01 11:51:52 -0400

    fixed file names and display names

--- src/constants/projectsPlaceholder.ts
+++ src/constants/projectsPlaceholder.ts
@@ -1,20 +1,23 @@
-export const projectsPlaceHolder: [[string, string, string][], string[]] = [
-  [
-    [
-      "Mark3d.png",
-      "Revolutionizing the secondhand online marketplace—with accessible 3D modelling",
-      "https://dorahacks.io/buidl/20367",
-    ],
-    [
-      "Blocks.png",
-      "Scratch-like building blocks for Cairo smart contracts",
-      "https://dorahacks.io/buidl/20342",
-    ],
-    [
-      "Bravo Dispatch.png",
-      "Computer aided dispatch (CAD)",
-      "https://dorahacks.io/buidl/20371",
-    ],
-  ],
-  ["last year winner", "delightful design", "ai-powered"],
+export const projectsPlaceHolder: [[string, string, string, string][], string[]] = [
+	[
+		[
+			"mark3d.png",
+			"Revolutionizing the secondhand online marketplace—with accessible 3D modelling",
+			"https://dorahacks.io/buidl/20367",
+			"Mark3d",
+		],
+		[
+			"blocks.png",
+			"Scratch-like building blocks for Cairo smart contracts",
+			"https://dorahacks.io/buidl/20342",
+			"Blocks",
+		],
+		[
+			"bravo-dispatch.png",
+			"Computer aided dispatch (CAD)",
+			"https://dorahacks.io/buidl/20371",
+			"Bravo Dispatch",
+		],
+	],
+	["last year winner", "delightful design", "ai-powered"],
 ];
