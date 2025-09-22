90c8863b54062dc9cbb1788de2a6340534dc9bd0
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2025-09-21 23:33:34 -0400

    Implement `scan` endpoint (#428)
    * Implement `scan` endpoint
    
    * Prettify code
    
    * Finish tests for scan
    
    * Prettify code
    
    ---------
    
    Co-authored-by: prettify-hackwestern[bot] <prettify-hackwestern[bot]@users.noreply.github.com>
    Co-authored-by: Hunter <hello@hunterchen.ca>

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -4,6 +4,7 @@ import { applicationRouter } from "./routers/application";
 import { authRouter } from "./routers/auth";
 import { preregistrationRouter } from "./routers/preregistration";
 import { reviewRouter } from "./routers/review";
+import { scavengerHuntRouter } from "./routers/scavenger-hunt";
 
 /**
  * This is the primary router for your server.
@@ -16,6 +17,7 @@ export const appRouter = createTRPCRouter({
   auth: authRouter,
   preregistration: preregistrationRouter,
   review: reviewRouter,
+  scavenger: scavengerHuntRouter,
 });
 
 // export type definition of API
