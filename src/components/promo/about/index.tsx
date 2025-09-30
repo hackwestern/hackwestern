b8330f3014134d08b271616c50012d48b9de50ac
William Liu <liu.wllm@gmail.com>
2025-09-29 22:58:49 -0400

    Add new icons (#451)
    * Add new icons
    
    * Prettify code
    
    * Change svg to png
    
    ---------
    
    Co-authored-by: prettify-hackwestern[bot] <prettify-hackwestern[bot]@users.noreply.github.com>
    Co-authored-by: Hunter <hello@hunterchen.ca>

--- src/components/promo/about/index.tsx
+++ src/components/promo/about/index.tsx
@@ -8,21 +8,50 @@ function About() {
   return (
     <CanvasComponent offset={coordinates.about}>
       <div className="mt-8 flex flex-col items-center justify-center space-y-4 pb-12 sm:-mt-12">
-        <div className="flex origin-center flex-col items-center justify-center space-y-8 transition-transform duration-300 ease-in-out">
+        <div className="flex origin-center flex-col items-center justify-center transition-transform duration-300 ease-in-out">
           <div className="relative">
             <div className="flex w-full max-w-full flex-col items-center justify-start gap-4 px-4 pb-0 pt-24">
               <div className="h-5 text-center font-jetbrains-mono text-base font-medium uppercase text-zinc-500">
                 About
               </div>
-              <div className="mb-12 text-center font-dico text-2xl font-medium text-indigo-950">
+              <div className="mb-8 text-center font-dico text-2xl font-medium text-indigo-950">
                 Create. Collaborate. Innovate.
               </div>
-
               <DraggableImage
-                className="absolute -left-[320px] bottom-[150px] w-24"
+                className="absolute -bottom-[200px] -left-[600px] w-36"
+                key="pencils"
+                src="/pencils.png"
+                alt="Pencils"
+                animate={{
+                  rotate: [2, -2],
+                  transition: {
+                    duration: 1,
+                    repeat: Infinity,
+                    repeatType: "mirror",
+                    ease: "easeInOut",
+                  },
+                }}
+              />
+              <DraggableImage
+                className="absolute -right-[300px] bottom-[190px] w-24"
                 key="lightbulb"
-                src="/lightbulb.svg"
+                src="/lightbulb.png"
                 alt="Lightbulb"
+                animate={{
+                  rotate: [2, -2],
+                  transition: {
+                    duration: 1,
+                    repeat: Infinity,
+                    repeatType: "mirror",
+                    ease: "easeInOut",
+                  },
+                }}
+              />
+              <DraggableImage
+                className="absolute -bottom-[400px] -right-[650px] w-24"
+                key="paintbrush"
+                src="/paintbrush.png"
+                alt="Paintbrush"
                 animate={{
                   rotate: [2, -2],
                   transition: {
@@ -61,9 +90,9 @@ function About() {
                 <div className="flex max-w-96 flex-col items-center justify-center gap-x-8 gap-y-2 p-8">
                   <div>
                     <motion.img
-                      src="/collab.png"
+                      src="/people.png"
                       alt="hackwestern"
-                      width={250}
+                      width={300}
                       height={150}
                       draggable="false"
                     />
@@ -76,7 +105,7 @@ function About() {
                   <div className="absolute -bottom-[10px] -right-[50px] z-10 h-20 w-40"></div>
                 </div>
               </div>
-              <div className="shrink-0">
+              <div className="shrink-0 pt-8">
                 <Envelope />
               </div>
             </div>
