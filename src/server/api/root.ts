1dd8bb3ee57a1ffd9f0771a7e0936a548e11663d
ethan-rng <ethan.rong@gmail.com>
2025-10-01 17:57:24 -0400

    removed extra import statement

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -5,7 +5,6 @@ import { authRouter } from "./routers/auth";
 import { preregistrationRouter } from "./routers/preregistration";
 import { scavengerHuntRouter } from "./routers/scavenger-hunt";
 import { reviewRouter } from "./routers/review";
-import { scavengerHuntRouter } from "./routers/scavenger-hunt";
 
 /**
  * This is the primary router for your server.
