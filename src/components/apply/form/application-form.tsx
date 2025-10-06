1f5b04c53adfa35e1927975891feb52522a39ddd
liuwllm <liu.wllm@gmail.com>
2025-10-05 23:20:01 -0400

    Add invalid state to application questions + update questions

--- src/components/apply/form/application-form.tsx
+++ src/components/apply/form/application-form.tsx
@@ -12,6 +12,7 @@ import { Textarea } from "~/components/ui/textarea";
 import { api } from "~/utils/api";
 import { useAutoSave } from "~/components/hooks/use-auto-save";
 import { applicationStepSaveSchema } from "~/schemas/application";
+import { text } from "stream/consumers";
 
 export function ApplicationForm() {
   const utils = api.useUtils();
@@ -41,8 +42,7 @@ export function ApplicationForm() {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <div className="flex w-full flex-wrap gap-2">
           <FormLabel className="w-full">
-            If you could have any superpower to help you during Hack Western,
-            what would it be and why? (30 to 150 words)
+            If your laptop suddenly gained consciousness, what do you think it would say about your working style and why? (30 to 150 words)
           </FormLabel>
           <FormField
             control={form.control}
@@ -54,10 +54,10 @@ export function ApplicationForm() {
                     {...field}
                     value={field.value ?? ""}
                     placeholder="Type your message here"
-                    variant="primary"
+                    variant={(field.value?.split(/\s+/).filter(Boolean).length ?? 0) <= 150 ? "primary" : "invalid"}
                   />
                 </FormControl>
-                <div className="text-sm text-gray-500">
+                <div className={`text-sm ${((field.value?.split(/\s+/).filter(Boolean).length ?? 0) <= 150) ? "text-gray-500" : "text-[#f76b7c]"}`}>
                   {field.value?.split(/\s+/).filter(Boolean).length ?? 0} / 150
                   words
                 </div>
@@ -67,8 +67,7 @@ export function ApplicationForm() {
         </div>
         <div className="flex w-full flex-wrap gap-2">
           <FormLabel className="w-full">
-            If you could build your own dream destination what would it look
-            like? Be as detailed and creative as you want! (30 to 150 words)
+            What’s one piece of feedback you’ve received that stuck with you and why? (30 to 150 words)
           </FormLabel>
           <FormField
             control={form.control}
@@ -80,10 +79,10 @@ export function ApplicationForm() {
                     {...field}
                     value={field.value ?? ""}
                     placeholder="Type your message here"
-                    variant="primary"
+                    variant={(field.value?.split(/\s+/).filter(Boolean).length ?? 0) <= 150 ? "primary" : "invalid"}
                   />
                 </FormControl>
-                <div className="text-sm text-gray-500">
+                <div className={`text-sm ${((field.value?.split(/\s+/).filter(Boolean).length ?? 0) <= 150) ? "text-gray-500" : "text-[#f76b7c]"}`}>
                   {field.value?.split(/\s+/).filter(Boolean).length ?? 0} / 150
                   words
                 </div>
@@ -93,9 +92,7 @@ export function ApplicationForm() {
         </div>
         <div className="flex w-full flex-wrap gap-2">
           <FormLabel className="w-full">
-            What project (anything you have ever worked on not just restricted
-            to tech) of yours are you the most proud of and why? What did you
-            learn throughout the process? (30 to 150 words)
+            What’s a project you’d love to revisit and improve if you had the time, and why? (30 to 150 words)
           </FormLabel>
           <FormField
             control={form.control}
@@ -107,10 +104,10 @@ export function ApplicationForm() {
                     {...field}
                     value={field.value ?? ""}
                     placeholder="Type your message here"
-                    variant="primary"
+                    variant={(field.value?.split(/\s+/).filter(Boolean).length ?? 0) <= 150 ? "primary" : "invalid"}
                   />
                 </FormControl>
-                <div className="text-sm text-gray-500">
+                <div className={`text-sm ${((field.value?.split(/\s+/).filter(Boolean).length ?? 0) <= 150) ? "text-gray-500" : "text-[#f76b7c]"}`}>
                   {field.value?.split(/\s+/).filter(Boolean).length ?? 0} / 150
                   words
                 </div>
