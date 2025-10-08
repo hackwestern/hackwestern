0e61f88780eb7280062c70c9d61ee08e79664174
Hunter Chen <hello@hunterchen.ca>
2025-10-08 18:16:00 -0400

    update schemas

--- drizzle/0013_confused_fenris.sql
+++ drizzle/0013_confused_fenris.sql
@@ -1,4 +1,5 @@
 ALTER TYPE "public"."country" ADD VALUE 'India' BEFORE 'Other';--> statement-breakpoint
+ALTER TABLE "hw_application" ADD COLUMN "canvas_data" jsonb;--> statement-breakpoint
 ALTER TABLE "hw_user" ADD COLUMN "scavenger_hunt_earned" integer DEFAULT 0;--> statement-breakpoint
 ALTER TABLE "hw_user" ADD COLUMN "scavenger_hunt_balance" integer DEFAULT 0;--> statement-breakpoint
 CREATE INDEX "user_scavenger_hunt_earned_idx" ON "hw_user" USING btree ("scavenger_hunt_earned");--> statement-breakpoint
--- drizzle/0013_mature_doctor_spectrum.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "hw_application" ADD COLUMN "canvas_description" text;--> statement-breakpoint
-ALTER TABLE "hw_application" ADD COLUMN "canvas_data" text;
