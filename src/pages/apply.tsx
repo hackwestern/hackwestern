5b0d109ceccce321cb0b1d0850a96a861901d50c
Hunter Chen <hello@hunterchen.ca>
2025-10-09 16:39:01 -0400

    fix avatar formatting

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -31,6 +31,9 @@ import Link from "next/link";
 import { signOut } from "next-auth/react";
 import { AvatarDisplay } from "~/components/apply/avatar-display";
 import { Drawer, DrawerContent, DrawerTrigger } from "~/components/ui/drawer";
+import Image from "next/image";
+import { motion, useAnimation } from "framer-motion";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 function getApplyStep(
   stepValue: string | null,
@@ -125,19 +128,39 @@ function DesktopCharacterIcon() {
 
 function MobileStickerDrawer() {
   const { data } = api.application.get.useQuery();
+  const controls = useAnimation();
+
+  async function handleStickerClick() {
+    await controls.start({
+      rotate: 180,
+      transition: { duration: 0.2, ease: "easeInOut" },
+    });
+    await controls.start({
+      rotate: 0,
+      transition: { duration: 0.2, ease: "easeInOut" },
+    });
+  }
 
   return (
     <div className="fixed bottom-20 right-4 z-50 overscroll-contain md:hidden">
       <Drawer direction="bottom">
         <DrawerTrigger asChild>
-          {/* eslint-disable @next/next/no-img-element */}
-          <img
-            src="/mobile-sticker.png"
-            alt="Sticker Drawer"
-            className="h-full w-full object-contain"
-          />
+          <motion.div
+            onClick={handleStickerClick}
+            animate={controls}
+            whileTap={{ scale: 0.96 }}
+            style={{ display: "inline-block", cursor: "pointer" }}
+          >
+            <Image
+              src="/mobile-sticker.png"
+              alt="Sticker Drawer"
+              width={64}
+              height={64}
+            />
+          </motion.div>
         </DrawerTrigger>
         <DrawerContent className="h-fit overflow-hidden overscroll-contain">
+          <DialogTitle className="sr-only">Your Stickers</DialogTitle>
           <div className="z-[100] mx-auto h-2 w-[100px] rounded-full bg-muted" />
           <div className="absolute inset-0 overflow-hidden rounded-t-xl">
             <CanvasBackground />
@@ -145,14 +168,14 @@ function MobileStickerDrawer() {
           <div className="relative h-full w-full overscroll-contain">
             <div className="relative z-10 flex flex-wrap items-center justify-center gap-8 p-6">
               {data?.avatarColour && (
-                <div className="self-center">
+                <div className="-ml-8 mb-4 mr-6 h-36 w-36 scale-50 self-center">
                   <AvatarDisplay
                     avatarColour={data?.avatarColour}
                     avatarFace={data?.avatarFace}
                     avatarLeftHand={data?.avatarLeftHand}
                     avatarRightHand={data?.avatarRightHand}
                     avatarHat={data?.avatarHat}
-                    size="sm"
+                    size="lg"
                   />
                 </div>
               )}
@@ -279,7 +302,7 @@ export default function Apply() {
 
                 {/* Main card */}
                 <div>
-                  <div className="flex h-lg w-md flex-col justify-start space-y-8 rounded-md bg-white px-8 py-8 shadow-lg sm:w-lg md:px-12 md:py-12 lg:w-3xl 2xl:h-[65vh] 2xl:w-4xl 3xl:h-[60vh] 3xl:w-6xl 4xl:w-7xl">
+                  <div className="h-lg flex w-md flex-col justify-start space-y-8 rounded-md bg-white px-8 py-8 shadow-lg sm:w-lg md:px-12 md:py-12 lg:w-3xl 2xl:h-[65vh] 2xl:w-4xl 3xl:h-[60vh] 3xl:w-6xl 4xl:w-7xl">
                     <div className="space-y-4 py-1.5">
                       <h1 className="font-dico text-2xl font-medium text-heavy">
                         {heading}
