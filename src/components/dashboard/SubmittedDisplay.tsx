7d3849fcbc57a0e1df776dd2056c25c102d511e2
Hunter Chen <hello@hunterchen.ca>
2025-10-10 16:41:29 -0400

    formatting looks like it works?

--- src/components/dashboard/SubmittedDisplay.tsx
+++ src/components/dashboard/SubmittedDisplay.tsx
@@ -89,7 +89,7 @@ export default function SubmittedDisplay({
 
   return (
     <div className="relative m-5 mr-2 flex flex-col items-center gap-6 rounded-lg p-10 xl:flex-row">
-      <div className="flex flex-col gap-6">
+      <div className="ml-8 flex flex-col gap-6">
         <h2 className="max-w-xl font-dico text-4xl font-semibold text-heavy">
           Your application has been submitted!
         </h2>
@@ -112,77 +112,79 @@ export default function SubmittedDisplay({
           </Button>
         </div>
       </div>
-      <div
-        ref={cardRef}
-        className="-pr-4 h-80 w-80 rounded-lg"
-        style={{
-          background: `${selectedColor?.bg} 30%`,
-        }}
-      >
-        <div className="relative h-80 w-80 rounded-lg">
-          {pathStrings.length > 0 && (
-            <svg className="h-80 w-80">
-              {pathStrings.map((pathString, pathIndex) => (
-                <path
-                  key={pathIndex}
-                  d={pathString}
-                  stroke={selectedColor?.gradient ?? "#a16bc7"}
-                  strokeWidth="8"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              ))}
-            </svg>
-          )}
-          <div className="absolute right-4 top-1/2 z-[1000] flex -translate-y-1/2 flex-col items-end gap-4">
-            <div className="-right-2 rounded-full border border-heavy px-2 py-1 font-jetbrains-mono text-xs">
-              HACK WESTERN XII
+      <div className="flex w-full justify-start lg:ml-16 lg:ml-8">
+        <div
+          ref={cardRef}
+          className="h-80 w-80 rounded-lg lg:mt-0"
+          style={{
+            background: `${selectedColor?.bg} 30%`,
+          }}
+        >
+          <div className="relative h-80 w-80 rounded-lg">
+            {pathStrings.length > 0 && (
+              <svg className="h-80 w-80">
+                {pathStrings.map((pathString, pathIndex) => (
+                  <path
+                    key={pathIndex}
+                    d={pathString}
+                    stroke={selectedColor?.gradient ?? "#a16bc7"}
+                    strokeWidth="8"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                ))}
+              </svg>
+            )}
+            <div className="absolute right-4 top-1/2 z-[1000] flex -translate-y-1/2 flex-col items-end gap-4">
+              <div className="-right-2 rounded-full border border-heavy px-2 py-1 font-jetbrains-mono text-xs">
+                HACK WESTERN XII
+              </div>
+              <h1 className="z-[1000] overflow-ellipsis text-right font-dico text-3xl text-heavy">
+                {`${application?.firstName ?? "Hacker"} ${application?.lastName ?? ""}`}
+              </h1>
             </div>
-            <h1 className="z-[1000] overflow-ellipsis text-right font-dico text-3xl text-heavy">
-              {`${application?.firstName ?? "Hacker"} ${application?.lastName ?? ""}`}
-            </h1>
-          </div>
-          {application?.avatarColour && (
-            <div className="absolute -top-4 left-24 -ml-8 mb-4 mr-6 h-36 w-36 scale-[0.3] self-center">
-              <AvatarDisplay
-                avatarColour={application?.avatarColour}
-                avatarFace={application?.avatarFace}
-                avatarLeftHand={application?.avatarLeftHand}
-                avatarRightHand={application?.avatarRightHand}
-                avatarHat={application?.avatarHat}
-                size="lg"
-              />
-            </div>
-          )}
-          <div className="absolute -right-10 bottom-[2] scale-[0.5]">
-            <SchoolStamp type={application?.school} />
-          </div>
-          <div className="absolute bottom-4 right-24 scale-[0.7]">
-            <MajorStamp type={application?.major} />
-          </div>
-          {application?.attendedBefore !== undefined &&
-            application?.attendedBefore !== null && (
-              <div className="absolute left-48 top-3 mr-1 scale-[0.85]">
-                <HWStamp
-                  returning={
-                    application?.attendedBefore ? "returnee" : "newcomer"
-                  }
+            {application?.avatarColour && (
+              <div className="absolute -top-4 left-24 -ml-8 mb-4 mr-6 h-36 w-36 scale-[0.3] self-center">
+                <AvatarDisplay
+                  avatarColour={application?.avatarColour}
+                  avatarFace={application?.avatarFace}
+                  avatarLeftHand={application?.avatarLeftHand}
+                  avatarRightHand={application?.avatarRightHand}
+                  avatarHat={application?.avatarHat}
+                  size="lg"
                 />
               </div>
             )}
-          <div className="absolute bottom-2 scale-[0.7]">
-            <HackerStamp numHackathons={application?.numOfHackathons} />
-          </div>
+            <div className="absolute -right-10 bottom-[2] scale-[0.5]">
+              <SchoolStamp type={application?.school} />
+            </div>
+            <div className="absolute bottom-4 right-24 scale-[0.7]">
+              <MajorStamp type={application?.major} />
+            </div>
+            {application?.attendedBefore !== undefined &&
+              application?.attendedBefore !== null && (
+                <div className="absolute left-48 top-3 mr-1 scale-[0.85]">
+                  <HWStamp
+                    returning={
+                      application?.attendedBefore ? "returnee" : "newcomer"
+                    }
+                  />
+                </div>
+              )}
+            <div className="absolute bottom-2 scale-[0.7]">
+              <HackerStamp numHackathons={application?.numOfHackathons} />
+            </div>
 
-          {application?.githubLink &&
-            application?.linkedInLink &&
-            application?.otherLink &&
-            application?.resumeLink && (
-              <div className="absolute left-6 top-8 -ml-4 scale-[0.9]">
-                <LinksStamp />
-              </div>
-            )}
+            {application?.githubLink &&
+              application?.linkedInLink &&
+              application?.otherLink &&
+              application?.resumeLink && (
+                <div className="absolute left-6 top-8 -ml-4 scale-[0.9]">
+                  <LinksStamp />
+                </div>
+              )}
+          </div>
         </div>
       </div>
     </div>
