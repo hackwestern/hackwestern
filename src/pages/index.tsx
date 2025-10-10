9bbe14cd23fdc73a8d7006bdd62ae8522a86e686
liuwllm <liu.wllm@gmail.com>
2025-10-10 11:31:45 -0400

    Revert "Add buttons"
    This reverts commit 458f1771328d6a7ad4fb8a71aaf0b3cc6c2a1672.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,6 @@ import FAQ from "~/components/promo/faq";
 import Team from "~/components/promo/team";
 import { coordinates } from "~/constants/canvas";
 import MLHTrustBadge from "~/components/promo/mlh-trust.badge";
-import { Button } from "~/components/ui/button";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -37,12 +35,6 @@ export default function Home() {
         </Canvas>
         <Footer />
         <MLHTrustBadge />
-        <Button
-          className="fixed right-24 top-6 z-50 w-fit md:right-28 md:top-10 lg:right-48 lg:top-14"
-          variant="primary"
-        >
-          <Link href="/login">Log In</Link>
-        </Button>
       </main>
     </>
   );
