38659d0cf00235867d96554e4aff7a98e5bc6b6e
liuwllm <liu.wllm@gmail.com>
2025-11-11 10:50:07 -0500

    Add mobile breakpoints for construction page

--- src/components/live/construction.tsx
+++ src/components/live/construction.tsx
@@ -1,7 +1,7 @@
 const Construction = () => {
   return (
     <div className="w-fit h-full flex flex-col">
-        <div className="flex flex-col items-center justify-center bg-highlight p-48 rounded-xl gap-4">
+        <div className="flex flex-col items-center justify-center bg-highlight p-24 md:p-48 rounded-xl gap-4">
             <p className="text-lg text-heavy font-jetbrains-mono">THIS PAGE IS UNDER CONSTRUCTION!</p>
             <p className="text-lg text-medium font-figtree">Please check again soon! :&#41;</p>
         </div>
