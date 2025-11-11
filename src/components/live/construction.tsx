9d79cb172b6f077b4ff4a4b5892ed8fba59ac7b8
liuwllm <liu.wllm@gmail.com>
2025-11-10 19:18:11 -0500

    Add temporary in construction page

---
+++ src/components/live/construction.tsx
@@ -0,0 +1,12 @@
+const Construction = () => {
+  return (
+    <div className="w-fit h-full flex flex-col">
+        <div className="flex flex-col items-center justify-center bg-highlight p-48 rounded-xl gap-4">
+            <p className="text-lg text-heavy font-jetbrains-mono">THIS PAGE IS UNDER CONSTRUCTION!</p>
+            <p className="text-lg text-medium font-figtree">Please check again soon! :&#41;</p>
+        </div>
+    </div>
+  );
+};
+
+export default Construction;
