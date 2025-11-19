4ac74bee5c29092d36d0488a8b02038b0c7e50b8
liuwllm <liu.wllm@gmail.com>
2025-11-19 18:29:48 -0500

    Remove class from svg

--- src/components/live/icons.tsx
+++ src/components/live/icons.tsx
@@ -288,7 +288,7 @@ const HousekeepingIcon = ({ selected }: { selected: boolean }) => {
 
 const ScavengerHuntIcon = ({ selected }: { selected: boolean }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={selected ? SELECTED : UNSELECTED} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={selected ? SELECTED : UNSELECTED} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
   );
 };
 
