28316e13626721420535a3b79fd0dafe2a25ea1e
Hunter Chen <hello@hunterchen.ca>
2025-11-20 17:33:56 -0500

    Update sponsors-live.ts

--- src/constants/sponsors-live.ts
+++ src/constants/sponsors-live.ts
@@ -79,14 +79,6 @@ export const SPONSOR_TIERS: SponsorTier[] = [
         description:
           "Manulife is a leading international financial services group that helps people make their decisions easier and lives better. We operate primarily as John Hancock in the United States and Manulife elsewhere. We provide financial advice, insurance, as well as wealth and asset management solutions for individuals, groups and institutions.",
       },
-      {
-        name: "Communications Security Establishment Canada",
-        // TODO: Add CSE logo
-        logo: "/sponsors/cse.png",
-        link: "https://www.cse-cst.gc.ca/",
-        description:
-          "Communications Security Establishment Canada (CSE) is the national cryptologic agency, providing the Government of Canada with information technology security and foreign signals intelligence. CSE student jobs are unlike most others. Our students work on meaningful projects that have an impact on the country, and they build personal connections that last beyond their student years. Our main objective is to retain our students as full-time employees after their studies.\n\nTo be part of CSE's student program you must be registered full-time in a Canadian college or university. Our students work on challenging, interesting and unique projects which help keep our country safe from cyber threats. Every employee plays a key role in protecting Canada and Canadians while working with some of the most advanced technologies in the world. Where else can you say that at the end of every workday?\n\nIf you are interested in a student position with CSE, please note that you must submit your application approximately eight months prior to the start of your desired placement. Students at CSE must go through a rigorous security clearance process that takes approximately six months to complete.",
-      },
     ],
   },
   {
