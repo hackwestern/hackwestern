06373f3cc79648598d7d0952332f9271c066415e
holiazhang <zhangholia@gmail.com>
2026-05-15 14:49:07 -0400

    updated text field

--- src/components/internals/text-field.tsx
+++ src/components/internals/text-field.tsx
@@ -1,34 +1,41 @@
 import { Input } from "~/components/ui/input";
 import PrimaryButton from "./primary-button";
+import { Skeleton } from "../ui/skeleton";
 
 interface TextFieldProps {
   children: string;
   submit?: boolean;
   secondary?: boolean;
+  isSkeleton?: boolean;
 }
 export default function TextField({
   children,
   submit = false,
   secondary = false,
+  isSkeleton = false,
 }: TextFieldProps) {
-  return (
+    if (isSkeleton) return(
+    <div className="flex items-center">
+        <Skeleton className="h-10 px-10 py-2 w-max text-transparent">{children}</Skeleton>
+        {submit && <Skeleton className="h-10 text-transparent w-max px-2 py-2">Submit</Skeleton>}
+    </div>
+    )
+  else return (
     <>
-      {submit ? (
-        <div
-          className={`flex w-max rounded-lg ${secondary ? "border bg-highlight" : "bg-white/50} border-2 border-white"}`}
-        >
-          <Input
-            placeholder={children}
-            className={"border-none bg-transparent"}
-          />
-          <PrimaryButton textField>Submit</PrimaryButton>
-        </div>
-      ) : (
-        <Input
-          placeholder={children}
-          className={`w-max rounded-lg border px-6 ${secondary ? "bg-highlight" : "border-none border-white bg-white/50"}`}
-        />
-      )}
+        {submit ? (
+            <div className={`flex w-max rounded-lg ${secondary ? "border bg-highlight" : "border-2 border-white bg-white/50"}`}>
+            <Input
+              placeholder={children}
+              className={"border-none bg-transparent"}
+            />
+            <PrimaryButton textField>Submit</PrimaryButton>
+            </div>
+        ) : (
+            <Input
+              placeholder={children}
+              className={`w-max rounded-lg border px-6 ${secondary ? "bg-highlight":"w-max border-none border-white bg-white/50"}`}            />
+        )
+        }
     </>
   );
 }
