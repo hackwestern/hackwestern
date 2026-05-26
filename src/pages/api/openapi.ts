3bf139df276b7d381bc167d0ca8ade5be46c3a4c
ethan-rng <ethan.rong@gmail.com>
2026-05-25 21:38:30 -0400

    working demo for api.json

--- src/pages/api/openapi.ts
+++ src/pages/api/openapi.ts
@@ -2,15 +2,12 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { generateOpenApiDocument } from "trpc-to-openapi";
 import { appRouter } from "~/server/api/root";
 
-export default async function GET(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const doc = generateOpenApiDocument(appRouter, {
     title: "tRPC OpenAPI",
     version: "1.0.0",
     baseUrl: "http://localhost:3000",
-    defs: {},
   });
 
-  console.log(doc);
-
-  return res.status(500);
+  return res.status(200).json(doc);
 }
