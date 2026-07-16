6b7b015a051d882bb584566c80b1fa98a03fe793
Lucas <lucasvanderwielen@icloud.com>
2026-07-16 19:52:10 -0400

    added teams endpoints and tests

---
+++ src/server/api/routers/teams.test.ts
@@ -0,0 +1,140 @@
+import { describe, expect, test, beforeAll, afterAll } from "vitest";
+import { mockSession } from "~/server/auth";
+import { db } from "~/server/db";
+import { createInnerTRPCContext } from "../trpc";
+import { createCaller } from "~/server/api/root";
+import { eq, inArray } from "drizzle-orm";
+import { teams, users } from "~/server/db/schema";
+import { faker } from "@faker-js/faker";
+
+const session = await mockSession(db);
+
+const ctx = createInnerTRPCContext({ session });
+const caller = createCaller(ctx);
+
+const insertTeam = async () => {
+  const id = faker.string.sample(6);
+  await db.insert(teams).values({ name: "Team 1", id: id });
+  return id;
+};
+const removeTeam = async (id: string) => {
+  await db.delete(teams).where(eq(teams.id, id));
+};
+
+describe("teams basic endpoints", () => {
+  describe("createTeam Tests", () => {
+    test("createTeam success", () => {
+      const res = caller.teams.createTeam({ name: "Best Team" });
+      return expect(res).resolves.toEqual({ success: true });
+    });
+    test("createTeam already in team", async () => {
+      const teamId = await insertTeam();
+      await db
+        .update(users)
+        .set({ teamId: teamId })
+        .where(eq(users.id, ctx.session!.user.id));
+      const res = caller.teams.createTeam({ name: "Best Team" });
+
+      await expect(res).rejects.toThrow();
+
+      await db
+        .update(users)
+        .set({ teamId: null })
+        .where(eq(users.id, ctx.session!.user.id));
+      await removeTeam(teamId);
+    });
+  });
+  describe.sequential("joinTeam Tests", () => {
+    let teamId: string;
+    beforeAll(async () => {
+      teamId = await insertTeam();
+    });
+    afterAll(async () => {
+      removeTeam(teamId);
+    });
+    test("joinTeam success", async () => {
+      const res = caller.teams.joinTeam({ teamId: teamId });
+
+      return expect(res).resolves.toEqual({ success: true });
+    });
+    test("joinTeam fail", async () => {
+      const res = caller.teams.joinTeam({ teamId: teamId });
+
+      return expect(res).rejects.toThrow();
+    });
+    test("joinTeam full", async () => {
+      const teamId = await insertTeam();
+
+      const s = () => {
+        return {
+          id: faker.string.uuid(),
+          name: faker.person.fullName(),
+          email: faker.internet.email(),
+          emailVerified: faker.date.anytime(),
+          image: faker.image.avatar(),
+          teamId: teamId,
+        };
+      };
+      const newUsers = Array.from({ length: 4 }, s);
+
+      await db.insert(users).values(newUsers);
+
+      const res = caller.teams.joinTeam({ teamId: teamId });
+      await expect(res).rejects.toThrow();
+
+      await db.delete(users).where(
+        inArray(
+          users.id,
+          newUsers.map((u) => u.id),
+        ),
+      );
+      await removeTeam(teamId);
+    });
+  });
+  describe.sequential("leaveTeam Tests", () => {
+    let teamId: string;
+    beforeAll(async () => {
+      teamId = await insertTeam();
+    });
+    afterAll(async () => {
+      removeTeam(teamId);
+    });
+    test("leaveTeam success", async () => {
+      await db
+        .update(users)
+        .set({ teamId: teamId })
+        .where(eq(users.id, ctx.session!.user.id));
+      const res = caller.teams.leaveTeam();
+
+      return expect(res).resolves.toEqual({ success: true });
+    });
+    test("leaveTeam fail", async () => {
+      const res = caller.teams.leaveTeam();
+
+      await expect(res).rejects.toThrow();
+    });
+  });
+  describe.sequential("deleteTeam Tests", () => {
+    let teamId: string;
+    beforeAll(async () => {
+      teamId = await insertTeam();
+    });
+    afterAll(async () => {
+      removeTeam(teamId);
+    });
+
+    test("deleteTeam success", async () => {
+      await db
+        .update(users)
+        .set({ teamId: teamId })
+        .where(eq(users.id, ctx.session!.user.id));
+
+      const res = caller.teams.deleteTeam();
+      await expect(res).resolves.toEqual({ success: true });
+    });
+    test("deleteTeam fail", async () => {
+      const res = caller.teams.deleteTeam();
+      await expect(res).rejects.toThrow();
+    });
+  });
+});
---
+++ src/server/api/routers/teams.ts
@@ -0,0 +1,149 @@
+import { TRPCError } from "@trpc/server";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import z from "zod";
+import { db } from "~/server/db";
+import { teams, users } from "~/server/db/schema";
+import { count, eq } from "drizzle-orm";
+import { randomBytes } from "crypto";
+
+export const teamsRouter = createTRPCRouter({
+  createTeam: protectedProcedure
+    .input(z.object({ name: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const team = await db.query.users.findFirst({
+        columns: { teamId: true },
+        where: eq(users.id, ctx.session.user.id),
+      });
+      if (!team) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "User not found",
+        });
+      }
+      if (team.teamId != null) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message:
+            "This user it already a part of a team, leave their current team to create a new one",
+        });
+      }
+      if (!input.name) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "A team name is required to create a team",
+        });
+      }
+
+      const id = randomBytes(3).toString("hex");
+
+      await db.insert(teams).values({
+        id: id,
+        name: input.name,
+      });
+      return {
+        success: true,
+      };
+    }),
+  joinTeam: protectedProcedure
+    .input(z.object({ teamId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const currentTeam = await db.query.users.findFirst({
+        columns: { teamId: true },
+        where: eq(users.id, ctx.session.user.id),
+      });
+      if (!currentTeam) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "User not found",
+        });
+      }
+      if (currentTeam.teamId != null) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message:
+            "This user it already a part of a team, leave their current team to join a new one",
+        });
+      }
+      if (!input.teamId) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "No specified teamId",
+        });
+      }
+      const team = await db.query.teams.findFirst({
+        where: eq(teams.id, input.teamId),
+      });
+      if (!team) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Team does not exist",
+        });
+      }
+      const [teamSize] = await db
+        .select({ value: count() })
+        .from(users)
+        .where(eq(users.teamId, input.teamId));
+      const size = teamSize?.value ?? 100;
+
+      if (size >= 4) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Team is full (max 4)",
+        });
+      }
+      await db
+        .update(users)
+        .set({ teamId: input.teamId })
+        .where(eq(users.id, ctx.session.user.id));
+      return { success: true };
+    }),
+  leaveTeam: protectedProcedure.query(async ({ ctx }) => {
+    const currentTeam = await db.query.users.findFirst({
+      columns: { teamId: true },
+      where: eq(users.id, ctx.session.user.id),
+    });
+    if (!currentTeam) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "User not found",
+      });
+    }
+    if (!currentTeam.teamId) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Cannot leave team because user is not in a team",
+      });
+    }
+    await db
+      .update(users)
+      .set({ teamId: null })
+      .where(eq(users.id, ctx.session.user.id));
+
+    return { success: true };
+  }),
+  deleteTeam: protectedProcedure.query(async ({ ctx }) => {
+    const currentTeam = await db.query.users.findFirst({
+      columns: { teamId: true },
+      where: eq(users.id, ctx.session.user.id),
+    });
+    if (!currentTeam) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "User not found",
+      });
+    }
+    if (!currentTeam.teamId) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Cannot delete team because user is not in a team",
+      });
+    }
+    await db
+      .update(users)
+      .set({ teamId: null })
+      .where(eq(users.teamId, currentTeam.teamId));
+    return {
+      success: true,
+    };
+  }),
+});
