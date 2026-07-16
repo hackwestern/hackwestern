21b89e20b503b0e8c90e47420971f42585db200e
holiazhang <zhangholia@gmail.com>
2026-07-16 11:19:25 -0400

    added a redirect

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -13,128 +13,137 @@ import { useToast } from "~/hooks/use-toast";
 import DiscordAuthButton from "~/components/auth/discordauth-button";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [pending, setPending] = useState(false);
-  const router = useRouter();
-  const { toast } = useToast();
+  return null;
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [pending, setPending] = useState(false);
+  // const router = useRouter();
+  // const { toast } = useToast();
 
-  // Get the callbackUrl from query params, default to /dashboard
-  const callbackUrl = (router.query.callbackUrl as string) ?? "/dashboard";
+  // // Get the callbackUrl from query params, default to /dashboard
+  // const callbackUrl = (router.query.callbackUrl as string) ?? "/dashboard";
 
-  async function handleSubmit(e: FormEvent) {
-    e.preventDefault();
-    setPending(true);
-    void signIn("credentials", {
-      username: email,
-      password,
-      redirect: false,
-    }).then((response) => {
-      if (response && response.ok === false) {
-        toast({
-          title: "Error",
-          description: "Invalid email or password",
-          variant: "destructive",
-        });
-        setPending(false);
-        return;
-      }
-      void router.push(callbackUrl);
-    });
-  }
+  // async function handleSubmit(e: FormEvent) {
+  //   e.preventDefault();
+  //   setPending(true);
+  //   void signIn("credentials", {
+  //     username: email,
+  //     password,
+  //     redirect: false,
+  //   }).then((response) => {
+  //     if (response && response.ok === false) {
+  //       toast({
+  //         title: "Error",
+  //         description: "Invalid email or password",
+  //         variant: "destructive",
+  //       });
+  //       setPending(false);
+  //       return;
+  //     }
+  //     void router.push(callbackUrl);
+  //   });
+  // }
 
-  return (
-    <>
-      <SEO
-        title="Sign In"
-        description="Sign in to your Hack Western account. Hack Western is one of Canada's largest student-run hackathons at Western University."
-      />
+  // return (
+  //   <>
+  //     <SEO
+  //       title="Sign In"
+  //       description="Sign in to your Hack Western account. Hack Western is one of Canada's largest student-run hackathons at Western University."
+  //     />
 
-      <div className="bg-hw-radial-gradient m-auto flex h-screen flex-col items-center justify-center">
-        <div className="z-10 mx-4 flex-col items-center rounded-lg bg-background p-8 text-sm shadow-md sm:w-xl sm:p-12 md:w-2xl md:text-base">
-          <h2 className="font-dico mb-4 self-start text-[30px] text-heavy md:text-[34px]">
-            Sign into your account
-          </h2>
-          <form onSubmit={handleSubmit}>
-            <h2 className="mb-1 font-figtree text-medium">Email</h2>
-            <Input
-              id="email"
-              name="email"
-              type="text"
-              autoComplete="username"
-              className="mb-4 h-[60px] bg-highlight text-medium"
-              placeholder="hello@hackwestern.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <h2 className="mb-1 font-figtree text-medium">Password</h2>
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              className="mb-8 h-[60px] bg-highlight text-medium"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <Button
-              variant="primary"
-              type="submit"
-              size="lg"
-              full
-              isPending={pending}
-            >
-              {pending ? "Signing In..." : "Sign In"}
-            </Button>
-          </form>
+  //     <div className="bg-hw-radial-gradient m-auto flex h-screen flex-col items-center justify-center">
+  //       <div className="z-10 mx-4 flex-col items-center rounded-lg bg-background p-8 text-sm shadow-md sm:w-xl sm:p-12 md:w-2xl md:text-base">
+  //         <h2 className="font-dico mb-4 self-start text-[30px] text-heavy md:text-[34px]">
+  //           Sign into your account
+  //         </h2>
+  //         <form onSubmit={handleSubmit}>
+  //           <h2 className="mb-1 font-figtree text-medium">Email</h2>
+  //           <Input
+  //             id="email"
+  //             name="email"
+  //             type="text"
+  //             autoComplete="username"
+  //             className="mb-4 h-[60px] bg-highlight text-medium"
+  //             placeholder="hello@hackwestern.com"
+  //             value={email}
+  //             onChange={(e) => setEmail(e.target.value)}
+  //             required
+  //           />
+  //           <h2 className="mb-1 font-figtree text-medium">Password</h2>
+  //           <Input
+  //             id="password"
+  //             name="password"
+  //             type="password"
+  //             autoComplete="current-password"
+  //             className="mb-8 h-[60px] bg-highlight text-medium"
+  //             placeholder="Enter your password"
+  //             value={password}
+  //             onChange={(e) => setPassword(e.target.value)}
+  //             required
+  //           />
+  //           <Button
+  //             variant="primary"
+  //             type="submit"
+  //             size="lg"
+  //             full
+  //             isPending={pending}
+  //           >
+  //             {pending ? "Signing In..." : "Sign In"}
+  //           </Button>
+  //         </form>
 
-          <div className="relative flex w-full items-center py-2 md:py-6">
-            <div className="flex-grow border-t border-gray-400 opacity-20" />
-            <span className="mx-4 flex-shrink text-gray-400">or</span>
-            <div className="flex-grow border-t border-gray-400 opacity-20" />
-          </div>
-          <div className="flex flex-col items-stretch gap-4">
-            <GoogleAuthButton redirect={callbackUrl} />
-            <GithubAuthButton redirect={callbackUrl} />
-            <DiscordAuthButton redirect={callbackUrl} />
-          </div>
-          <div className="mt-6 font-figtree text-medium">
-            Don&apos;t have an account yet?
-            <Button
-              asChild
-              variant="tertiary"
-              className="ml-2 h-max p-0 text-sm md:text-base"
-            >
-              <Link
-                className="text-purple-500 hover:text-violet-700"
-                href="/register"
-              >
-                Create Account
-              </Link>
-            </Button>
-          </div>
-          <div className="font-figtree text-medium">
-            Forget password?
-            <Button
-              asChild
-              variant="tertiary"
-              className="ml-2 h-max p-0 text-sm md:text-base"
-            >
-              <Link
-                className="text-purple-500 hover:text-violet-700"
-                href="/forgot-password"
-              >
-                Reset Password
-              </Link>
-            </Button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  //         <div className="relative flex w-full items-center py-2 md:py-6">
+  //           <div className="flex-grow border-t border-gray-400 opacity-20" />
+  //           <span className="mx-4 flex-shrink text-gray-400">or</span>
+  //           <div className="flex-grow border-t border-gray-400 opacity-20" />
+  //         </div>
+  //         <div className="flex flex-col items-stretch gap-4">
+  //           <GoogleAuthButton redirect={callbackUrl} />
+  //           <GithubAuthButton redirect={callbackUrl} />
+  //           <DiscordAuthButton redirect={callbackUrl} />
+  //         </div>
+  //         <div className="mt-6 font-figtree text-medium">
+  //           Don&apos;t have an account yet?
+  //           <Button
+  //             asChild
+  //             variant="tertiary"
+  //             className="ml-2 h-max p-0 text-sm md:text-base"
+  //           >
+  //             <Link
+  //               className="text-purple-500 hover:text-violet-700"
+  //               href="/register"
+  //             >
+  //               Create Account
+  //             </Link>
+  //           </Button>
+  //         </div>
+  //         <div className="font-figtree text-medium">
+  //           Forget password?
+  //           <Button
+  //             asChild
+  //             variant="tertiary"
+  //             className="ml-2 h-max p-0 text-sm md:text-base"
+  //           >
+  //             <Link
+  //               className="text-purple-500 hover:text-violet-700"
+  //               href="/forgot-password"
+  //             >
+  //               Reset Password
+  //             </Link>
+  //           </Button>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 }
 
-export const getServerSideProps = hackerLoginRedirect;
+// export const getServerSideProps = hackerLoginRedirect;
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: "/",
+      permanent: false,
+    },
+  };
+};
